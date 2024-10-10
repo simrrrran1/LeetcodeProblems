8136f2ef8b643ffe40ecb493b9092e73b722f1c4
simrrrran1 <91774565+simrrrran1@users.noreply.github.com>
2024-10-11 00:10:57 +0530

    C++ Solution
            Submission Statistics:
            Question #: 300
            Language: C++
            Runtime: 68 ms
            Runtime percentile: 65.29
            Memory: 13.204 MB
            Memory percentile: 51.42

---
+++ 300. Longest Increasing Subsequence/Solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,0);
+        dp[0]=1;
+        int ans=1;
+        for(int i=1;i<n;i++){
+            for(int j=i-1;j>=0;j--){
+                if(nums[j]>=nums[i])continue;
+                dp[i]=max(dp[i],dp[j]);
+            }
+            dp[i]+=1;
+            ans=max(ans,dp[i]);
+        }
+        return ans;
+    }
+};
