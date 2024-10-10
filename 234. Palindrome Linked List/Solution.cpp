79d60a9ca8526f789decf26206d804bcac6479b7
simrrrran1 <91774565+simrrrran1@users.noreply.github.com>
2024-10-10 23:58:40 +0530

    C++ Solution
            Submission Statistics:
            Question #: 234
            Language: C++
            Runtime: 153 ms
            Runtime percentile: 80.52
            Memory: 116.76 MB
            Memory percentile: 91.50

---
+++ 234. Palindrome Linked List/Solution.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseLL(ListNode* head){
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        ListNode* temp=NULL;
+        while(curr){
+            temp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        if(!head || !head->next)return true;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* revHead=reverseLL(slow);
+        while(head && revHead){
+            if(head->val!=revHead->val)
+                return false;
+            head=head->next;
+            revHead=revHead->next;
+        }
+        return true;
+    }
+};
