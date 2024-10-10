9e22c2205b21292c3d30cce11828484147e96e85
simrrrran1 <91774565+simrrrran1@users.noreply.github.com>
2024-10-11 00:12:31 +0530

    C++ Solution
            Submission Statistics:
            Question #: 9
            Language: C++
            Runtime: 4 ms
            Runtime percentile: 85.27
            Memory: 10.548 MB
            Memory percentile: 12.71

---
+++ 9. Palindrome Number/Solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)return false;
+        string s=to_string(x);
+        int i=0,j=s.size()-1;
+        while(i<=j){
+            if(s[i]!=s[j])return false;
+            i++;j--;
+        }
+        return true;
+    }
+};
