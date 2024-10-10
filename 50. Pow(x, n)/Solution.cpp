828d9fffbb6c78993f8ae3b81d06ddeb744c12a9
simrrrran1 <91774565+simrrrran1@users.noreply.github.com>
2024-10-11 00:12:53 +0530

    C++ Solution
            Submission Statistics:
            Question #: 50
            Language: C++
            Runtime: 4 ms
            Runtime percentile: 22.97
            Memory: 8.524 MB
            Memory percentile: 8.70

---
+++ 50. Pow(x, n)/Solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        // int a=abs(n);
+        // double ans=pow(x,a);
+        // return n<0?1/ans:ans;
+        return pow(x,n);
+    }
+};
