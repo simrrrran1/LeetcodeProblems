9655b30d8ef87d01f07da8093763dc9e875a9837
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2024-10-11 17:33:39 +0530

    C++ Solution
            Submission Statistics:
            Question #: 15
            Language: C++
            Runtime: 53 ms
            Runtime percentile: 90.07
            Memory: 27.532 MB
            Memory percentile: 51.89

--- 15. 3Sum/Solution.cpp
+++ 15. 3Sum/Solution.cpp
@@ -1,5 +1,16 @@
 class Solution {
 public:
+    void twoPointer(vector<int>&nums,int start, int end,int i, vector<vector<int>>&ans){
+        while(start<end){
+            int sum=nums[i]+nums[start]+nums[end];
+            if(sum==0){
+                ans.push_back({nums[i],nums[start],nums[end]});
+                while(start<end && nums[start]==nums[++start]);
+                while(start<end && nums[end]==nums[--end]);
+            }else if(sum<0)start++;
+            else end--;
+        }
+    }
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n=nums.size();
         if(n<3)return {};
@@ -7,16 +18,7 @@ public:
         sort(nums.begin(),nums.end());
         for(int i=0;i<n-2;i++){
             if(i>0 && nums[i]==nums[i-1])continue;
-            int start=i+1,end=n-1;
-            while(start<end){
-                int sum=nums[i]+nums[start]+nums[end];
-                if(sum==0){
-                    ans.push_back({nums[i],nums[start],nums[end]});
-                    while(start<end && nums[start]==nums[++start]);
-                    while(start<end && nums[end]==nums[--end]);
-                }else if(sum<0)start++;
-                else end--;
-            }
+            twoPointer(nums,i+1,n-1,i,ans);
         }
         return ans;
     }
