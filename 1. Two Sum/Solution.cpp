18238e82b204fdccb93a7b124227ebd35529dd30
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-01-14 00:50:28 +0530

    C++ Solution
          Submission Statistics:
          Question #: 1
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 14.904 MB
          Memory percentile: 18.94

--- 1. Two Sum/Solution.cpp
+++ 1. Two Sum/Solution.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> mp;
+        //keep target-nums[i] track in map for better search
+        unordered_map<int,int> mp; 
+
         for(int i=0;i<nums.size();i++){
-            int comp=target-nums[i];
-            if(mp.find(comp)!=mp.end()){
-                return {mp[comp],i};
+            int k = target-nums[i];
+            // to avoid repetition of solution, implemented mp[nums[i]]!=i
+            if(mp.find(k)!=mp.end()){
+                return {mp[k],i};
             }
             mp[nums[i]]=i;
         }
+
         return {};
     }
 };
