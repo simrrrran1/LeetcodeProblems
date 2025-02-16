c3074155ba1971764688edb2bc8bd8647ed6a210
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-02-16 15:25:10 +0530

    C++ Solution
          Submission Statistics:
          Question #: 394
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 9.192 MB
          Memory percentile: 79.89

--- 394. Decode String/Solution.cpp
+++ 394. Decode String/Solution.cpp
@@ -1,41 +1,68 @@
 class Solution {
 public:
+
     bool isDigit(char s){
         if(s=='0'||s=='1'||s=='2'||s=='3'||s=='4'||s=='5'||s=='6'||s=='7'||s=='8'||s=='9')return true;
         return false;
     }
+
     string decodeString(string s) {
-        stack<char>s1;
-        for(int i=0;i<s.size();i++){
-            if(s[i]==']'){
-                string s2="";
-                while(s1.top()!='['){
-                    s2=s1.top()+s2;
-                    s1.pop();
-                }
-                s1.pop();
-                string k="";
-                while(!s1.empty() && isDigit(s1.top())){
-                    k=s1.top()+k;
-                    s1.pop();
-                }
-                int x=stoi(k);
-                string temp=s2;
-                for(int i=0;i<x-1;i++){
-                    s2+=temp;
-                }
-                for(int i=0;i<s2.size();i++)
-                s1.push(s2[i]);
-            }else{
+        string ans = "";
+        stack<char> s1;
+
+        for(int i=0; i<s.size(); i++){
+
+            if(s[i]!=']'){
                 s1.push(s[i]);
             }
+            else{
+
+                //decode curr string
+                string curr = "";
+                while(!s1.empty() &&  s1.top()!='[' ){
+                    curr = s1.top() + curr;
+                    s1.pop();
+                }
+
+                s1.pop(); //remove [
+
+
+                //decode the number of repetitions
+                string num = "";
+                while(!s1.empty() && isDigit(s1.top()) ){
+                    num = s1.top() + num;
+                    s1.pop();
+                }
+
+                //convert string to number k
+                int k = stoi(num);
+                // int k = 0;
+                // for(int i=0; i<num.size(); i++){
+                //     k = (k * 10) + (num[i] - '0');
+                // }
+
+                //copy temp to curr
+                string temp = curr;
+                for(int i=0; i<k-1; i++){
+                    curr += temp;
+                }
+
+                //push curr to stack
+                for(int i=0; i<curr.size(); i++){
+                    s1.push(curr[i]);
+                }
+
+            }
+
+
         }
-        string ans="";
+
         while(!s1.empty()){
             ans+=s1.top();
             s1.pop();
         }
         reverse(ans.begin(),ans.end());
+
         return ans;
     }
 };
