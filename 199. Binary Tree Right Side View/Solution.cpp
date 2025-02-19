aa69e5d77f7901af25f59a6a5989014a479cfcd1
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-02-19 12:05:17 +0530

    C++ Solution
          Submission Statistics:
          Question #: 199
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 15.044 MB
          Memory percentile: 43.10

---
+++ 199. Binary Tree Right Side View/Solution.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        if(!root)return {};
+        vector<int> ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int k = q.size();
+            for(int i=0; i<k; i++){
+
+                TreeNode* front = q.front();
+                q.pop();
+                
+                if(i==0)ans.push_back(front->val);
+                
+                if(front -> right){
+                    q.push(front ->right);
+                }
+                if(front ->left){
+                    q.push(front ->left);
+                }
+
+            }
+        }
+        return ans;
+    }
+};
