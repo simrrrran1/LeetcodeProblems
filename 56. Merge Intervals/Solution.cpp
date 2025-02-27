0ce6f40cb7753ffce9a6a3595032c655d823c695
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-02-27 19:06:21 +0530

    C++ Solution
          Submission Statistics:
          Question #: 56
          Language: C++
          Runtime: 3 ms
          Runtime percentile: 89.76
          Memory: 24.004 MB
          Memory percentile: 27.46

--- 56. Merge Intervals/Solution.cpp
+++ 56. Merge Intervals/Solution.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>>ans;
-        sort(intervals.begin(),intervals.end());
-        pair<int,int>p;
-        p.first=intervals[0][0];
-        p.second=intervals[0][1];
-        for(int i=1;i<intervals.size();i++){
-            if(p.second<intervals[i][0]){
-                ans.push_back({p.first,p.second});
-                p.first=intervals[i][0];
-                p.second=intervals[i][1];
+        vector<vector<int>> ans;
+        pair<int, int> p;
+        sort(intervals.begin(), intervals.end());
+        p.first = intervals[0][0];
+        p.second = intervals[0][1];
+
+        for(int i=1; i<intervals.size(); i++){
+            if(intervals[i][0] > p.second){
+                ans.push_back({p.first, p.second});
+                p.first = intervals[i][0];
+                p.second = intervals[i][1];
             }else{
-                p.second=max(intervals[i][1],p.second);
+                p.second = max(p.second, intervals[i][1]);
             }
         }
-        ans.push_back({p.first,p.second});
+        ans.push_back({p.first, p.second});
         return ans;
     }
 };
