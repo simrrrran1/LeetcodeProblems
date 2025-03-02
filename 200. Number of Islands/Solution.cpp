b15a752654044a59fbc30dea7c70aea467214bbb
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-03-02 23:32:48 +0530

    C++ Solution
          Submission Statistics:
          Question #: 200
          Language: C++
          Runtime: 21 ms
          Runtime percentile: 91.76
          Memory: 16.108 MB
          Memory percentile: 88.92

--- 200. Number of Islands/Solution.cpp
+++ 200. Number of Islands/Solution.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    void helper(vector<vector<char>>& grid,int i,int j,int n, int m){
-        if(i<0||j<0||i>=n||j>=m||grid[i][j]!='1')return;
-        grid[i][j]='2';
-        helper(grid,i-1,j,n,m);
-        helper(grid,i+1,j,n,m);
-        helper(grid,i,j-1,n,m);
-        helper(grid,i,j+1,n,m);
+    void dfs(vector<vector<char>>& grid, int row, int col, int m, int n){
+        if(row<0 || col<0 || row>=n || col>=m || grid[row][col]!='1')return;
+        grid[row][col] = '2';
+
+        dfs(grid, row-1, col, m, n);
+        dfs(grid, row+1, col, m, n);
+        dfs(grid, row, col-1, m, n);
+        dfs(grid, row, col+1, m, n);
     }
     int numIslands(vector<vector<char>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1'){
-                    helper(grid,i,j,n,m);
+        int ans = 0;
+        int n = grid.size();
+        int m =grid[0].size();
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j] == '1'){
+                    dfs(grid, i, j, m, n);
                     ans++;
                 }
             }
         }
+
         return ans;
     }
 };
