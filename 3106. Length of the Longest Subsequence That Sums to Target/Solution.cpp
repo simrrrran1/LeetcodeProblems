f2439fd0aca18c912d7105bffc197100f549808f
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-03-04 21:51:26 +0530

    C++ Solution
          Submission Statistics:
          Question #: 3106
          Language: C++
          Runtime: 181 ms
          Runtime percentile: 63.60
          Memory: 157.924 MB
          Memory percentile: 29.89

---
+++ 3106. Length of the Longest Subsequence That Sums to Target/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLongestSubsequence(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(target+1,-1));
+        for(int i=0; i<=n; i++) dp[i][0] = 0;
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=target; j++){
+
+                dp[i][j] = dp[i-1][j];
+                if(j >= nums[i - 1] && dp[i - 1][j - nums[i - 1]] != -1)
+                dp[i][j] = max(dp[i][j], 1+dp[i-1][j-nums[i-1]]);
+            }
+        }
+
+
+        return dp[n][target];
+    }
+};
