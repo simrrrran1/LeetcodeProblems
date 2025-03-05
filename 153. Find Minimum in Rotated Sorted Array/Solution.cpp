5d65d1662037c5a4bf49c507ba4162509a538243
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-03-05 22:09:10 +0530

    C++ Solution
          Submission Statistics:
          Question #: 153
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 14.1 MB
          Memory percentile: 78.97

--- 153. Find Minimum in Rotated Sorted Array/Solution.cpp
+++ 153. Find Minimum in Rotated Sorted Array/Solution.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int n=nums.size();
-        int i=0,j=n-1;
-        int ans=nums[0];
-        while(i<=j){
-            if(nums[i]<nums[j]){
-                ans=min(ans,nums[i]);break;
+        int ans = nums[0];
+        int left = 0, right = nums.size()-1;
+
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            if(nums[mid] >= nums[left]){
+                ans = min(ans, nums[left]);
+                left = mid+1;
+            }else{
+                ans = min(ans, nums[mid]);
+                right = mid-1;
             }
-            int mid=i+(j-i)/2;
-            ans=min(ans,nums[mid]);
-            if(nums[mid]>=nums[i])i=mid+1;
-            else j=mid-1;
         }
+
         return ans;
     }
 };
