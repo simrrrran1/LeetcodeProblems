6620b8a520912ed877a369cdb748bacce2c62fc2
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-03-05 19:12:40 +0530

    C++ Solution
          Submission Statistics:
          Question #: 238
          Language: C++
          Runtime: 1 ms
          Runtime percentile: 52.70
          Memory: 40.116 MB
          Memory percentile: 79.65

--- 238. Product of Array Except Self/Solution.cpp
+++ 238. Product of Array Except Self/Solution.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> v(n,0);
-        v[0]=1;
-        for(int i=1;i<n;i++){
-            v[i]=v[i-1]*nums[i-1];
+        int n = nums.size();
+        vector<int> ans(n,0);
+        ans[0] = 1;
+
+        for(int i=1; i<n; i++){
+            ans[i] = ans[i-1] * nums[i-1];
         }
-        int x=1;
-        for(int i=n-1;i>=0;i--){
-            v[i]=v[i]*x;
-            x*=nums[i];
+        int x = 1;
+        for(int i=n-1; i>=0; i--){
+            ans[i] *= x;
+            x *= nums[i];
         }
-        return v;
+
+        return ans;
     }
 };
