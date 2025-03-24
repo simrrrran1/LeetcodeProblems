b0ddd58ea8021a9262a4039656ad835eb430fa17
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-03-24 13:09:20 +0530

    C++ Solution
          Submission Statistics:
          Question #: 75
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 11.66 MB
          Memory percentile: 40.43

--- 75. Sort Colors/Solution.cpp
+++ 75. Sort Colors/Solution.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int zero = 0, one = 0, two = nums.size()-1;
-
-        while(one <= two){
-            if(nums[one] == 0){
-                swap(nums[one], nums[zero]);
-                zero++;one++;
-            }else if(nums[one] == 1){
-                one++;
-            }else if(nums[one] == 2){
-                swap(nums[one], nums[two]);
-                two--;
+        int left = 0, mid = 0, right = nums.size()-1;
+        while(mid <= right){
+            if(nums[mid] == 0){
+                swap(nums[left], nums[mid]);
+                left++;mid++;
+            }else if(nums[mid] == 1){
+                mid++;
+            }else if(nums[mid] == 2){
+                swap(nums[right], nums[mid]);
+                right--;
             }
-            
         }
     }
 };
