f979c0c5e1575ee4dce7a1aea68c17572e9fb6ac
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-03-24 12:51:48 +0530

    C++ Solution
          Submission Statistics:
          Question #: 15
          Language: C++
          Runtime: 47 ms
          Runtime percentile: 72.95
          Memory: 29.008 MB
          Memory percentile: 68.56

--- 15. 3Sum/Solution.cpp
+++ 15. 3Sum/Solution.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
-    void twoPointer(vector<int>&nums,int start, int end,int i, vector<vector<int>>&ans){
-        while(start<end){
-            int sum=nums[i]+nums[start]+nums[end];
-            if(sum==0){
-                ans.push_back({nums[i],nums[start],nums[end]});
-                while(start<end && nums[start]==nums[++start]);
-                //while(start<end && nums[start]==nums[start+1])start++;
-                while(start<end && nums[end]==nums[--end]);
-            }else if(sum<0)start++;
-            else end--;
-        }
-    }
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n=nums.size();
-        if(n<3)return {};
-        vector<vector<int>>ans;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n-2;i++){
-            if(i>0 && nums[i]==nums[i-1])continue;
-            twoPointer(nums,i+1,n-1,i,ans);
+        vector<vector<int>> ans;
+
+        sort(nums.begin(), nums.end());
+
+        for(int i=0; i<nums.size()-2; i++){
+            if(i>0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            int left = i+1, right = nums.size()-1;
+
+            while(left < right){
+                int sum = nums[left]+nums[right]+nums[i];
+                
+                if(sum==0){
+                    ans.push_back({nums[i], nums[left], nums[right]});
+                    while(left<right && nums[left] == nums[++left]);
+                    while(left<right && nums[right] == nums[--right]);
+
+                }else if(sum < 0){
+                    left++;
+                }else{
+                    right--;
+                }
+            }
         }
+
         return ans;
     }
 };
