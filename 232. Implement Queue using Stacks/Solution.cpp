6a5e864f2704f2c3217704428f5257007b3d7985
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-03-25 19:10:12 +0530

    C++ Solution
          Submission Statistics:
          Question #: 232
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 9.712 MB
          Memory percentile: 26.69

--- 232. Implement Queue using Stacks/Solution.cpp
+++ 232. Implement Queue using Stacks/Solution.cpp
@@ -1,58 +1,38 @@
 class MyQueue {
 public:
-    stack<int>s1;
-    stack<int>s2;
+    stack<int> s1;
+    stack<int> s2;
     MyQueue() {
         
     }
     
     void push(int x) {
-        if(s1.empty()){
-            while(!s2.empty()){
-                s1.push(s2.top());
-                s2.pop();
-            }
-        }
-        s1.push(x);
-    }
-    
-    int pop() {
-        // while(!s1.empty()){
-        //     s2.push(s1.top());
-        //     s1.pop();
-        // }
         if(s2.empty()){
             while(!s1.empty()){
                 s2.push(s1.top());
                 s1.pop();
             }
         }
-        int x=s2.top();
-        s2.pop();
-        // while(!s2.empty()){
-        //     s1.push(s2.top());
-        //     s2.pop();
-        // }
+        s2.push(x);
+    }
+    
+    int pop() {
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
+        }
+        int x = s1.top();
+        s1.pop();
         return x;
     }
     
     int peek() {
-        // while(!s1.empty()){
-        //     s2.push(s1.top());
-        //     s1.pop();
-        // }
-        if(s2.empty()){
-            while(!s1.empty()){
-                s2.push(s1.top());
-                s1.pop();
-            }
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
         }
-        int x=s2.top();
-        // while(!s2.empty()){
-        //     s1.push(s2.top());
-        //     s2.pop();
-        // }
-        return x;
+        return s1.top();
+
     }
     
     bool empty() {
