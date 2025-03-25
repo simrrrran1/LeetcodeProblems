718dccfa54edafcab3f6ca5d49d35d0360d72de9
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-03-25 12:27:41 +0530

    C++ Solution
          Submission Statistics:
          Question #: 234
          Language: C++
          Runtime: 4 ms
          Runtime percentile: 54.20
          Memory: 118.076 MB
          Memory percentile: 71.23

--- 234. Palindrome Linked List/Solution.cpp
+++ 234. Palindrome Linked List/Solution.cpp
@@ -10,32 +10,35 @@
  */
 class Solution {
 public:
-    ListNode* reverseLL(ListNode* head){
-        ListNode* prev=NULL;
-        ListNode* curr=head;
-        ListNode* temp=NULL;
+    ListNode* reverse(ListNode* head){
+        ListNode* temp = head;
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+
         while(curr){
-            temp=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=temp;
+            temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
         }
         return prev;
     }
     bool isPalindrome(ListNode* head) {
-        if(!head || !head->next)return true;
-        ListNode* slow=head;
-        ListNode* fast=head;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
         while(fast && fast->next){
-            slow=slow->next;
-            fast=fast->next->next;
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        ListNode* revHead=reverseLL(slow);
+        ListNode* revHead = reverse(slow);
+
         while(head && revHead){
-            if(head->val!=revHead->val)
+            if(head->val != revHead->val){
                 return false;
-            head=head->next;
-            revHead=revHead->next;
+            }
+            head = head->next;
+            revHead = revHead->next;
         }
         return true;
     }
