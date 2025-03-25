0f23b900415007f1e47bbc6c35dce3753e4ae0d7
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-03-25 18:32:17 +0530

    C++ Solution
          Submission Statistics:
          Question #: 225
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 9.372 MB
          Memory percentile: 85.31

---
+++ 225. Implement Stack using Queues/Solution.cpp
@@ -0,0 +1,35 @@
+class MyStack {
+public:
+    deque<int> dq;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        //dq.push_front(x);
+        dq.push_back(x);
+    }
+    
+    int pop() {
+        int last = dq.back();
+        dq.pop_back();
+        return last;
+    }
+    
+    int top() {
+        return dq.back();
+    }
+    
+    bool empty() {
+        return dq.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
