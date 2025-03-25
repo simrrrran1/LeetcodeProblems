2036d1df6ee2693e0b9b798e851009174f782185
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-03-25 12:32:45 +0530

    C++ Solution
          Submission Statistics:
          Question #: 141
          Language: C++
          Runtime: 8 ms
          Runtime percentile: 80.78
          Memory: 11.672 MB
          Memory percentile: 96.40

--- 141. Linked List Cycle/Solution.cpp
+++ 141. Linked List Cycle/Solution.cpp
@@ -11,13 +11,13 @@ public:
     bool hasCycle(ListNode *head) {
         ListNode* slow = head;
         ListNode* fast = head;
-
         while(fast && fast->next){
             slow = slow->next;
             fast = fast->next->next;
-            if(slow == fast)return true;
+            if(slow == fast){
+                return true;
+            }
         }
-
         return false;
     }
 };
