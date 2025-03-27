0f6225ee59818f0dcce3e3e32d0b64d3a0848873
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-03-27 13:17:00 +0530

    C++ Solution
          Submission Statistics:
          Question #: 2
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 77.124 MB
          Memory percentile: 43.24

---
+++ 2. Add Two Numbers/Solution.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry = 0;
+        ListNode* head = new ListNode(0);
+        ListNode* temp = head;
+
+        while(l1 || l2 || carry){
+            int sum = 0;
+            if(l1){
+                sum += (l1->val);
+                l1 = l1->next;
+            }
+            if(l2){
+                sum += (l2->val);
+                l2 = l2->next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            sum %= 10;
+            ListNode* temp1 = new ListNode(sum);
+            temp->next = temp1;
+            temp = temp->next;
+        }
+
+        return head->next;
+    }
+};
