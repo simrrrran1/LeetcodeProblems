06b61f3151d652752d23e0b4b7842edbbf905ccb
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-03-27 13:00:46 +0530

    C++ Solution
          Submission Statistics:
          Question #: 121
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 97.192 MB
          Memory percentile: 99.00

--- 121. Best Time to Buy and Sell Stock/Solution.cpp
+++ 121. Best Time to Buy and Sell Stock/Solution.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int maxProfit=0;
-        int minPrice=INT_MAX;
-        for(int i=0;i<prices.size();i++){
-            minPrice=min(minPrice,prices[i]);
-            maxProfit=max(maxProfit,prices[i]-minPrice);
+        int maxP = INT_MIN;
+        int minD = INT_MAX;
+
+        for(int i=0; i<prices.size(); i++){
+            minD = min(minD, prices[i]);
+            maxP = max(maxP, prices[i] - minD);
         }
-        return maxProfit;
+        return maxP;
     }
 };
