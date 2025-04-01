2017a6591ce644299432fc6365f43ee1b3a8415d
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-01 18:10:41 +0530

    C++ Solution
          Submission Statistics:
          Question #: 190
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 7.748 MB
          Memory percentile: 62.59

--- 190. Reverse Bits/Solution.cpp
+++ 190. Reverse Bits/Solution.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        int ans=0;
-        for(int i=0;i<32;i++){
-            uint32_t rem=(n>>i)&1;
-            ans|=(rem<<(31-i));
+        uint32_t revN = 0;
+        for(int i=0; i<32; i++){
+            revN = revN << 1;
+            int bit = n & 1;
+            revN = revN | bit;
+            n = n >> 1;
         }
-        return ans;
+        return revN;
     }
 };
