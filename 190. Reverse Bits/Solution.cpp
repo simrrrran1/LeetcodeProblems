fe7bd3aa40154a92832451ee4772fef19a5724ab
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-01 18:12:01 +0530

    C++ Solution
          Submission Statistics:
          Question #: 190
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 7.856 MB
          Memory percentile: 27.94

--- 190. Reverse Bits/Solution.cpp
+++ 190. Reverse Bits/Solution.cpp
@@ -4,12 +4,12 @@ public:
         uint32_t revN = 0;
         for(int i=0; i<32; i++){
 
-            revN = revN * 2 + (n % 2);
-            n /= 2;
-            // revN = revN << 1;
-            // int bit = n & 1;
-            // revN = revN | bit;
-            // n = n >> 1;
+            // revN = revN * 2 + (n % 2);
+            // n /= 2;
+            revN = revN << 1;
+            int bit = n & 1;
+            revN = revN | bit;
+            n = n >> 1;
         }
         return revN;
     }
