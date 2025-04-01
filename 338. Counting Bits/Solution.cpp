8ebdf56ac94955ae885ee8552d2ceb66d2ce7ac0
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-01 17:04:35 +0530

    C++ Solution
          Submission Statistics:
          Question #: 338
          Language: C++
          Runtime: 3 ms
          Runtime percentile: 24.29
          Memory: 10.52 MB
          Memory percentile: 80.19

--- 338. Counting Bits/Solution.cpp
+++ 338. Counting Bits/Solution.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> dp(n+1,0);
-        int offset=1;
-        for(int i=1;i<=n;i++){
-            if(offset*2==i)offset=i;
-            dp[i]=1+dp[i-offset];
+        vector<int> v(n+1, 0);
+        for(int i=0; i<=n; i++){
+            int hammingWeight = 0;
+            int x = i;
+            while(x){
+                hammingWeight += (x&1);
+                x /= 2;
+            }
+            v[i] = hammingWeight;
         }
-        return dp;
+
+        return v;
     }
 };
