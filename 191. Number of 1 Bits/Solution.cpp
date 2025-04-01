c828e73a4ae4749ac229340687e8f7235955f3be
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-01 16:57:57 +0530

    C++ Solution
          Submission Statistics:
          Question #: 191
          Language: C++
          Runtime: 3 ms
          Runtime percentile: 2.87
          Memory: 8.508 MB
          Memory percentile: 12.01

--- 191. Number of 1 Bits/Solution.cpp
+++ 191. Number of 1 Bits/Solution.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        int ans=0;
-        while(n!=0){
-            int rem=n%2;
-            if(rem==1)ans++;
-            n/=2;
+        vector<int> b(32,0);
+        int cnt = 0;
+
+        for(int i=31; i>=0; i--){
+            b[i] = n%2;
+            if(b[i] == 1) cnt++;
+            n /= 2;
         }
-        return ans;
+
+        return cnt;
     }
 };
