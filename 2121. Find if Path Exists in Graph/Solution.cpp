72f2b55c1cdb3442dfce9164fc0dbff9168394b1
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-04 18:14:38 +0530

    C++ Solution
          Submission Statistics:
          Question #: 2121
          Language: C++
          Runtime: 269 ms
          Runtime percentile: 76.48
          Memory: 298.56 MB
          Memory percentile: 66.69

--- 2121. Find if Path Exists in Graph/Solution.cpp
+++ 2121. Find if Path Exists in Graph/Solution.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
-    bool dfs(vector<int>&vis, vector<vector<int>>& adjList, int start, int end){
-        if(start == end) return true;
-        vis[start] = 1;
-        for(auto &i:adjList[start]){
-            if(!vis[i]){
-                if(dfs(vis, adjList, i, end))
+    bool helper(vector<vector<int>>& adj, vector<bool>&vis, int source, int destination){
+        if(source == destination) return true;
+        vis[source] = true;
+
+        for(auto& q: adj[source]){
+            if(!vis[q]){
+                if(helper(adj, vis, q, destination)){
                     return true;
+                }
             }
         }
+
         return false;
+
     }
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        vector<vector<int>> adjList(n);
-        for(int i=0; i<edges.size(); i++){
-            adjList[edges[i][0]].push_back(edges[i][1]);
-            adjList[edges[i][1]].push_back(edges[i][0]);
-        }
-        vector<int> vis(n,0);
-        return dfs(vis, adjList, source, destination);
+        vector<vector<int>> adj(n);
 
+        for(int i=0; i<edges.size(); i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+        }
+
+        vector<bool> vis(n, false);
+
+        return helper(adj, vis, source, destination);
     }
 };
