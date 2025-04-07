0fd203040c0bd933186aa2a3ec1f06acaf387405
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-07 12:57:44 +0530

    C++ Solution
          Submission Statistics:
          Question #: 463
          Language: C++
          Runtime: 3 ms
          Runtime percentile: 75.26
          Memory: 102.348 MB
          Memory percentile: 41.24

--- 463. Island Perimeter/Solution.cpp
+++ 463. Island Perimeter/Solution.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+    int dfs(vector<vector<int>>& grid, int i, int j, int row, int col){
+        if(i<0 || j<0 || i>=row || j>=col || grid[i][j] == 0) {
+            return 1;
+        }
+
+        if(grid[i][j] == 2) return 0;
+
+        grid[i][j] = 2;
+
+        int peri = 0;
+
+        peri += dfs(grid, i-1, j, row, col);
+        peri += dfs(grid, i+1, j, row, col);
+        peri += dfs(grid, i, j-1, row, col);
+        peri += dfs(grid, i, j+1, row, col);
+
+        return peri;
+    }
     int islandPerimeter(vector<vector<int>>& grid) {
         int row = grid.size();
         int col = grid[0].size();
@@ -9,9 +27,7 @@ public:
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
                 if(grid[i][j] == 1){
-                    perimeter += 4;
-                    if(i>0 && grid[i-1][j] == 1) perimeter -= 2;
-                    if(j>0 && grid[i][j-1] == 1) perimeter -= 2;
+                    perimeter = dfs(grid, i, j, row, col);
                 }
             }
         }
