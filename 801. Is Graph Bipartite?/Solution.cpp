3fd672741d7c7a60e523fe0419e7fbc6bc12867d
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-08 16:50:41 +0530

    C++ Solution
          Submission Statistics:
          Question #: 801
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 17.192 MB
          Memory percentile: 78.39

--- 801. Is Graph Bipartite?/Solution.cpp
+++ 801. Is Graph Bipartite?/Solution.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& graph, int node, vector<int>& colour){
-        for(auto& i: graph[node]){
-            if(colour[i] == -1){
-                colour[i] = 1-colour[node];
-                if(!dfs(graph, i, colour)){
-                    return false; 
+    bool helper(vector<vector<int>>& graph, int start, int x, vector<int>& color){
+        color[start] = x;
+        for(auto& i: graph[start]){
+            if(color[i] == -1){
+                if(!helper(graph, i, !x, color)){
+                    return false;
                 }
             }
-            else if(colour[i] == colour[node]){
-                return false;
-            }
+            else if(color[i] == color[start]) return false;
         }
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> colour(n, -1);
+        vector<int> color(n, -1);
+
         for(int i=0; i<n; i++){
-            if(colour[i] == -1){
-                colour[i] = 1;
-                if(!dfs(graph, i, colour)){
-                    return false; 
+            if(color[i] == -1){
+                if(!helper(graph, i, 0, color)){
+                    return false;
                 }
             }
         }
+
         return true;
     }
 };
