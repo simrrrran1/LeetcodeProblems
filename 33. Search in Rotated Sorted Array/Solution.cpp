cb9b6abe23b3463bcb9c52a39df173b4261101c9
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-08 23:13:29 +0530

    C++ Solution
          Submission Statistics:
          Question #: 33
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 15.268 MB
          Memory percentile: 33.79

--- 33. Search in Rotated Sorted Array/Solution.cpp
+++ 33. Search in Rotated Sorted Array/Solution.cpp
@@ -5,26 +5,23 @@ public:
 
         while(left <= right){
             int mid = left + (right-left)/2;
-            // [4,5,6,7,0,1,2]
-            if(nums[mid] == target) return mid;
-            
-            else if(nums[mid] >= nums[left]){
-                if(nums[left] <= target && target<= nums[mid]){
-                    right = mid-1;
-                }else{
-                    left = mid+1;
-                }
-            }
 
-            else{
-                if(nums[mid] <= target && target <= nums[right]){
-                    left = mid+1;
+            if(nums[mid] == target) return mid;
+
+            if(nums[mid] >= nums[left]){
+                if(nums[left] <= target && target <= nums[mid]){
+                    right = mid - 1;
                 }else{
-                    right = mid-1;
+                    left = mid + 1;
+                }
+            }else{
+                if(nums[mid] <= target && target <= nums[right]){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
                 }
             }
         }
-
         return -1;
     }
 };
