ce590370debe7ae8f4eee8d24653ee3a2815e944
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-08 18:12:14 +0530

    C++ Solution
          Submission Statistics:
          Question #: 695
          Language: C++
          Runtime: 2 ms
          Runtime percentile: 59.82
          Memory: 27.268 MB
          Memory percentile: 97.86

--- 695. Max Area of Island/Solution.cpp
+++ 695. Max Area of Island/Solution.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
+    int dfs(vector<vector<int>>& grid, int i, int j, int row, int col){
+        if(i<0 || j<0 || i>=row || j>=col || grid[i][j] != 1) return 0;
 
-    int dfs(vector<vector<int>>& grid, int row, int col, int n, int m){
-        if(row<0 || row>=n || col<0 || col>=m || !grid[row][col])return 0;
+        grid[i][j] = 2;
 
-        grid[row][col] = 0;
-
-        return 1 + dfs(grid, row+1, col, n, m) + dfs(grid, row-1, col, n, m) + dfs(grid, row, col+1, n, m)+ dfs(grid, row, col-1, n, m);
-
+        return 1 + dfs(grid, i-1, j, row, col) + dfs(grid, i+1, j, row, col) + dfs(grid, i, j-1, row, col) + dfs(grid, i, j+1, row, col);
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m =grid[0].size();
+        int row = grid.size();
+        int col = grid[0].size();
 
         int ans = 0;
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(grid[i][j])
-                ans = max(ans, dfs(grid, i, j, n, m));
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(grid[i][j] == 1){
+                    int sum = dfs(grid, i, j, row, col);
+                    ans = max(sum, ans);
+                }
             }
         }
+
 
         return ans;
     }
