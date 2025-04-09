5043189aac90e457a4b2381c071b3adc49ea8fb7
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-09 11:30:57 +0530

    C++ Solution
          Submission Statistics:
          Question #: 540
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 26.28 MB
          Memory percentile: 33.66

--- 540. Single Element in a Sorted Array/Solution.cpp
+++ 540. Single Element in a Sorted Array/Solution.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int x = nums[0];
-        for(int i=1; i<nums.size(); i++){
-            x = x^nums[i];
+        int n = nums.size();
+
+        int left = 0, right = n-1;
+        while(left <= right){
+            int mid = left + ((right-left)/2);
+            if((mid-1 < 0 || nums[mid] != nums[mid-1]) && (mid+1 == n || nums[mid] != nums[mid+1])){
+                return nums[mid];
+            }
+            int leftLen = nums[mid-1] == nums[mid] ? mid-1 : mid;
+
+            if(leftLen % 2 == 0){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
         }
-        return x;
+        return -1;
     }
 };
