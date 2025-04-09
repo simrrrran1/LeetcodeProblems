d40f67c424a130e0d58647e65022c8bd23e00155
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-09 19:17:14 +0530

    C++ Solution
          Submission Statistics:
          Question #: 11
          Language: C++
          Runtime: 4 ms
          Runtime percentile: 22.05
          Memory: 62.916 MB
          Memory percentile: 48.29

--- 11. Container With Most Water/Solution.cpp
+++ 11. Container With Most Water/Solution.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int n = height.size();
-        int left = 0, right = n-1;
-        int areaMax = 0;
+        int maxArea = 0;
+
+        int left = 0, right = height.size()-1;
 
         while(left < right){
             int h = min(height[left], height[right]);
             int w = right - left;
-            int area = h * w;
-            areaMax = max(area, areaMax);
+            maxArea = max(maxArea, h * w);
+
             if(height[left] < height[right]){
                 left++;
             }else{
                 right--;
             }
         }
-        return areaMax;
+
+
+        return maxArea;
     }
 };
