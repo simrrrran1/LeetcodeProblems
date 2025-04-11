db5ba2b5dd031b3246f92426eaea91ef93933f30
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-11 11:42:51 +0530

    C++ Solution
          Submission Statistics:
          Question #: 1014
          Language: C++
          Runtime: 71 ms
          Runtime percentile: 50.22
          Memory: 84.064 MB
          Memory percentile: 21.36

--- 1014. K Closest Points to Origin/Solution.cpp
+++ 1014. K Closest Points to Origin/Solution.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
- vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         vector<vector<int>> ans;
-        priority_queue<pair<double,vector<int>>,vector<pair<double,vector<int>>>,greater<pair<double,vector<int>>>>pq;
-        for(auto i:points){
-            int x=i[0];
-            int y=i[1];
-            double dist=sqrt((x*x)+(y*y));
-            pq.push({dist,{x,y}});
-        }
+
+        priority_queue<pair<double, vector<int>>, vector<pair<double, vector<int>>>, greater<pair<double, vector<int>>>> pq;
+
+        for(int i=0; i<points.size(); i++){
+            double dis = sqrt( pow(points[i][0], 2) + pow(points[i][1], 2) );
+            pq.push({dis, points[i]});
+        } 
+
         while(k--){
             ans.push_back(pq.top().second);
             pq.pop();
-        }
+        }  
+
         return ans;
     }
 };
