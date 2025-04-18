aef948cb21b635075d5ec481427587264cbe2bc1
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-19 00:26:15 +0530

    C++ Solution
          Submission Statistics:
          Question #: 42
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 26.04 MB
          Memory percentile: 61.21

--- 42. Trapping Rain Water/Solution.cpp
+++ 42. Trapping Rain Water/Solution.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-        int left = 0, right = n-1;
-        int maxLeft = height[0], maxRight = height[n-1];
+        int left = 0, right = height.size()-1;
+        int leftMax = 0, rightMax = 0;
         int ans = 0;
 
         while(left < right){
-            if(height[left] <= height[right]){
-                if(maxLeft < height[left]){
-                    maxLeft = height[left];
+            if(height[left] < height[right]){
+                if(height[left] > leftMax){
+                    leftMax = height[left];
                 }else{
-                    ans += (maxLeft - height[left]);
-                    left++;
+                    ans += (leftMax - height[left]);
                 }
+                left++;
             }else{
-                if(maxRight < height[right]){
-                    maxRight = height[right];
+                if(height[right] > rightMax){
+                    rightMax = height[right];
                 }else{
-                    ans += (maxRight - height[right]);
-                    right--;
+                    ans += (rightMax - height[right]);
                 }
+                right--;
             }
         }
-
         return ans;
     }
 };
