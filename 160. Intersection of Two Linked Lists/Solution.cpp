991a36e3bafe48f9acb1c9c16be874cd03eb063e
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-23 23:27:21 +0530

    C++ Solution
          Submission Statistics:
          Question #: 160
          Language: C++
          Runtime: 35 ms
          Runtime percentile: 71.13
          Memory: 18.488 MB
          Memory percentile: 82.72

--- 160. Intersection of Two Linked Lists/Solution.cpp
+++ 160. Intersection of Two Linked Lists/Solution.cpp
@@ -9,46 +9,53 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if(!headA && !headB)return headA;
-        if(!headA) return headB;
-        if(!headB) return headA;
+        if(!headA || !headB)return NULL;
+        //if(!headA) return headB;
+        //if(!headB) return headA;
         
-        int lA = 0, lB = 0;
+        //int lA = 0, lB = 0;
         ListNode* tempA = headA;
         ListNode* tempB = headB;
 
-        while(tempA){
-            lA++;
-            tempA = tempA->next;
-        }
-
-        while(tempB){
-            lB++;
-            tempB = tempB->next;
-        }
-
-        tempA = headA;
-        tempB = headB;
-
-        
-        if(lB > lA){
-            for(int i=0; i < lB-lA; i++){
-                tempB = tempB->next;
-            }
-        }else{
-            for(int i=0; i < lA-lB; i++){
-                tempA = tempA->next;
-            }
-        }
-        while(tempA || tempB){
-            if(tempA == tempB){
-                break;
-            }
-            tempA = tempA->next;
-            tempB = tempB->next;
+        while(tempA != tempB){
+            tempA = tempA ? tempA->next : headB;
+            tempB = tempB ? tempB->next : headA;
         }
 
         return tempA;
+
+        // while(tempA){
+        //     lA++;
+        //     tempA = tempA->next;
+        // }
+
+        // while(tempB){
+        //     lB++;
+        //     tempB = tempB->next;
+        // }
+
+        // tempA = headA;
+        // tempB = headB;
+
+        
+        // if(lB > lA){
+        //     for(int i=0; i < lB-lA; i++){
+        //         tempB = tempB->next;
+        //     }
+        // }else{
+        //     for(int i=0; i < lA-lB; i++){
+        //         tempA = tempA->next;
+        //     }
+        // }
+        // while(tempA || tempB){
+        //     if(tempA == tempB){
+        //         break;
+        //     }
+        //     tempA = tempA->next;
+        //     tempB = tempB->next;
+        // }
+
+        // return tempA;
 
     }
 };
