7acdea8752583fcb8cbc0e14d37ddbe77c821d15
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-26 10:55:07 +0530

    C++ Solution
          Submission Statistics:
          Question #: 210
          Language: C++
          Runtime: 11 ms
          Runtime percentile: 13.91
          Memory: 19.336 MB
          Memory percentile: 12.28

--- 210. Course Schedule II/Solution.cpp
+++ 210. Course Schedule II/Solution.cpp
@@ -1,47 +1,47 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& adj, stack<int>& s, unordered_set<int>& s1, int i, vector<bool>& vis){
-        if(vis[i]) return false;
-        s1.insert(i);
-        vis[i] = true;
+    bool dfs(vector<vector<int>>& adj, int start, vector<bool>& vis, stack<int>& s, unordered_set<int>& s1){
+        if(vis[start]) return false;
+        vis[start] =true;
+        s1.insert(start);
 
-        for(auto& it:adj[i]){
-            if(!vis[it]){
-                if(!dfs(adj, s, s1, it, vis)){
+        for(auto& i: adj[start]){
+            if(!vis[i]){
+                if(!dfs(adj, i, vis, s, s1)){
                     return false;
                 }
-            }else if(s1.count(it)) return false;
+            }else if(s1.find(i) != s1.end()) return false;
         }
-
-        s1.erase(i);
-        s.push(i);
+        s1.erase(start);
+        s.push(start);
         return true;
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        int n = prerequisites.size();
+        vector<int> ans;
+
         vector<vector<int>> adj(numCourses);
-        
-        for(int i=0; i<n; i++){
+
+        for(int i=0; i<prerequisites.size(); i++){
             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
 
-        vector<bool> vis(numCourses, 0);
         stack<int> s;
+        vector<bool> vis(numCourses, false);
 
         for(int i=0; i<numCourses; i++){
-            unordered_set<int> s1;
             if(!vis[i]){
-                if(!dfs(adj, s, s1, i, vis)){
+                unordered_set<int> s1; // to remember current path
+                if(!dfs(adj, i, vis, s, s1)){
                     return {};
                 }
             }
         }
 
-        vector<int> v;
         while(!s.empty()){
-            v.push_back(s.top());
+            ans.push_back(s.top());
             s.pop();
         }
-        return v;
+
+        return ans;
     }
 };
