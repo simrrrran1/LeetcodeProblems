c9bafd580c622579c7be30f9b7213e4107ec2349
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-27 15:18:24 +0530

    C++ Solution
          Submission Statistics:
          Question #: 394
          Language: C++
          Runtime: 2 ms
          Runtime percentile: 27.24
          Memory: 9.172 MB
          Memory percentile: 80.50

--- 394. Decode String/Solution.cpp
+++ 394. Decode String/Solution.cpp
@@ -1,68 +1,44 @@
 class Solution {
 public:
-
-    bool isDigit(char s){
-        if(s=='0'||s=='1'||s=='2'||s=='3'||s=='4'||s=='5'||s=='6'||s=='7'||s=='8'||s=='9')return true;
-        return false;
-    }
-
     string decodeString(string s) {
-        string ans = "";
+        int n = s.size();
         stack<char> s1;
-
-        for(int i=0; i<s.size(); i++){
-
-            if(s[i]!=']'){
-                s1.push(s[i]);
-            }
-            else{
-
-                //decode curr string
-                string curr = "";
-                while(!s1.empty() &&  s1.top()!='[' ){
-                    curr = s1.top() + curr;
+        for(int i=0; i<n; i++){
+            if(s[i] == ']'){
+                
+                string temp = "";
+                while(!s1.empty() && s1.top() != '['){
+                    temp = s1.top() + temp;
                     s1.pop();
                 }
-
-                s1.pop(); //remove [
-
-
-                //decode the number of repetitions
+                s1.pop();
+                
                 string num = "";
-                while(!s1.empty() && isDigit(s1.top()) ){
+                while(!s1.empty() && isdigit(s1.top())){
                     num = s1.top() + num;
                     s1.pop();
                 }
-
-                //convert string to number k
                 int k = stoi(num);
-                // int k = 0;
-                // for(int i=0; i<num.size(); i++){
-                //     k = (k * 10) + (num[i] - '0');
-                // }
-
-                //copy temp to curr
-                string temp = curr;
-                for(int i=0; i<k-1; i++){
-                    curr += temp;
+                
+                string x = "";
+                while(k--){
+                    x += temp;
                 }
-
-                //push curr to stack
-                for(int i=0; i<curr.size(); i++){
-                    s1.push(curr[i]);
+                
+                for(int i=0; i<x.size(); i++){
+                    s1.push(x[i]);
                 }
-
+                continue;
             }
-
-
+            s1.push(s[i]);
         }
+        string ans = "";
 
         while(!s1.empty()){
-            ans+=s1.top();
+            ans.push_back(s1.top());
             s1.pop();
         }
-        reverse(ans.begin(),ans.end());
-
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
