382098b0844e180a66605f3bff2280db0f02fe9a
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-27 18:05:01 +0530

    C++ Solution
          Submission Statistics:
          Question #: 713
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 65.088 MB
          Memory percentile: 95.14

---
+++ 713. Subarray Product Less Than K/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if(k <= 1) return 0;
+        int ans = 0;
+        int pro = 1;
+        int left = 0;
+        int right = 0;
+        while(right < nums.size()){
+            pro *= nums[right];
+            while(pro >= k){
+                pro /= nums[left];
+                left++;
+            }
+            ans += (right - left + 1);
+            right++;
+        }
+        return ans;
+    }
+};
