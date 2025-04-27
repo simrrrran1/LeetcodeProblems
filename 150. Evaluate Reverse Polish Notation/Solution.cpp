327de9fc0edb875ebadbf911872a336d2a4583ce
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-27 13:05:30 +0530

    C++ Solution
          Submission Statistics:
          Question #: 150
          Language: C++
          Runtime: 1 ms
          Runtime percentile: 53.39
          Memory: 17.168 MB
          Memory percentile: 36.95

--- 150. Evaluate Reverse Polish Notation/Solution.cpp
+++ 150. Evaluate Reverse Polish Notation/Solution.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
+    
     int evalRPN(vector<string>& tokens) {
+        int n = tokens.size();
         stack<int> s;
-        for(int i=0;i<tokens.size();i++){
-            if(tokens[i]=="+"||tokens[i]=="-"||tokens[i]=="*"||tokens[i]=="/"){
-                int a=s.top();
+
+        for(int i=0; i<n; i++){
+            if(tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/"){
+                int a = s.top();
                 s.pop();
-                int b=s.top();
+                int b = s.top();
                 s.pop();
-                if(tokens[i]=="+")s.push(a+b);
-                else if(tokens[i]=="-")s.push(b-a);
-                else if(tokens[i]=="*")s.push(a*b);
-                else s.push(b/a);
-            }else s.push(stoi(tokens[i]));
+                if(tokens[i] == "+"){
+                    s.push(a+b);
+                }else if(tokens[i] == "-"){
+                    s.push(b-a);
+                }else if(tokens[i] == "*"){
+                    s.push(a*b);
+                }else{
+                    s.push(b/a);
+                }
+            }else {
+                s.push(stoi(tokens[i]));
+            }
         }
         return s.top();
     }
