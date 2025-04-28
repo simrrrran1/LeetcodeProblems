b168459f1ecf8f582bbf65aa481cfeaf4a455cec
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-28 11:53:04 +0530

    C++ Solution
          Submission Statistics:
          Question #: 416
          Language: C++
          Runtime: 124 ms
          Runtime percentile: 42.52
          Memory: 17.356 MB
          Memory percentile: 48.06

--- 416. Partition Equal Subset Sum/Solution.cpp
+++ 416. Partition Equal Subset Sum/Solution.cpp
@@ -2,15 +2,20 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
+
         int sum = 0;
+
         for(int i: nums){
             sum += i;
         }
 
-        if(sum % 2 != 0) return false;
-        sum = sum/2;
+        if(sum % 2 == 1) return false;
+        sum /= 2;
+
 
         vector<vector<bool>> dp(n+1, vector<bool>(sum+1, false));
+
+        // if sum == 0
 
         for(int i=0; i<=n; i++){
             dp[i][0] = true;
@@ -22,11 +27,11 @@ public:
                     dp[i][j] = dp[i-1][j];
                 }else{
                     dp[i][j] = (dp[i-1][j] || dp[i-1][j-nums[i-1]]);
+
                 }
             }
         }
 
         return dp[n][sum];
-
     }
 };
