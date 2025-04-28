fd181ace2a252679454e767879fc3d067b6019be
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-28 11:48:41 +0530

    C++ Solution
          Submission Statistics:
          Question #: 416
          Language: C++
          Runtime: 125 ms
          Runtime percentile: 42.19
          Memory: 17.44 MB
          Memory percentile: 47.59

--- 416. Partition Equal Subset Sum/Solution.cpp
+++ 416. Partition Equal Subset Sum/Solution.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
-    bool helper(vector<int>&nums, int sum){
-        int n=nums.size();
-        bool dp[n+1][sum+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=sum;j++){
-                if(i==0)dp[i][j]=false;
-                if(j==0)dp[i][j]=true;
-            }
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=sum;j++){
-                if(nums[i-1]<=j)dp[i][j]=dp[i-1][j-nums[i-1]]||dp[i-1][j];
-                else dp[i][j]=dp[i-1][j];
-            }
-        }
-        return dp[n][sum];
-    }
     bool canPartition(vector<int>& nums) {
-        int sum=0;
-        for(int i:nums)sum+=i;
-        if(sum%2!=0)return false;
-        return helper(nums,sum/2);
+        int n = nums.size();
+        int sum = 0;
+        for(int i: nums){
+            sum += i;
+        }
+
+        if(sum % 2 != 0) return false;
+        sum = sum/2;
+
+        vector<vector<bool>> dp(n+1, vector<bool>(sum+1, false));
+
+        for(int i=0; i<=n; i++){
+            dp[i][0] = true;
+        }
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=sum; j++){
+                if(j < nums[i-1]){
+                    dp[i][j] = dp[i-1][j];
+                }else{
+                    dp[i][j] = (dp[i-1][j] || dp[i-1][j-nums[i-1]]);
+                }
+            }
+        }
+
+        return dp[n][sum];
+
     }
 };
