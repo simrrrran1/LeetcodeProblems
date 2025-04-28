59ec327436e62aaa7b4fb3e89d9ef1ee81d9d8d3
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-28 12:17:16 +0530

    C++ Solution
          Submission Statistics:
          Question #: 45
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 20.272 MB
          Memory percentile: 98.09

--- 45. Jump Game II/Solution.cpp
+++ 45. Jump Game II/Solution.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int n=nums.size();
-        if(n<2)return 0;
-        int ans=0;
-        int left=0,right=0;
-        while(right<n-1){
-            int maxIdx=0;
-            for(int i=left;i<=right&&i<n;i++){
-                maxIdx=max(maxIdx,i+nums[i]);
-            }
-            left=right+1;
-            right=maxIdx;
-            ans++;
-        }
-        return ans;
+        int left = 0, right = 0, ans = 0;
 
+        while(right < nums.size()-1){
+            int farthest = 0;
+            for(int i=left; i<=right; i++){
+                farthest = max(farthest, i+nums[i]);
+            }
+            left = right+1;
+            right = farthest;
+            ans += 1;
+        }
+
+        return ans;
     }
 };
