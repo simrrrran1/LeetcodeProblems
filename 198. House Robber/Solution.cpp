941c06c7c002157a07ecf234048985fb2b7355ef
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-04-28 13:08:30 +0530

    C++ Solution
          Submission Statistics:
          Question #: 198
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 10.208 MB
          Memory percentile: 71.26

--- 198. House Robber/Solution.cpp
+++ 198. House Robber/Solution.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int rob1 = 0, rob2 = 0;
+
         for(int i=0; i<nums.size(); i++){
             int temp = max(rob1 + nums[i], rob2);
             rob1 = rob2;
             rob2 = temp;
         }
-        return rob2;
+        return rob2; //hold the final result because of line 9
     }
 };
