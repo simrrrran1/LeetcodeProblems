5abddc13603bfa9116fd44dcec830fdcbad1f16c
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-02 20:56:55 +0530

    C++ Solution
          Submission Statistics:
          Question #: 907
          Language: C++
          Runtime: 3 ms
          Runtime percentile: 95.74
          Memory: 22.816 MB
          Memory percentile: 74.21

--- 907. Koko Eating Bananas/Solution.cpp
+++ 907. Koko Eating Bananas/Solution.cpp
@@ -2,32 +2,30 @@ class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int n = piles.size();
-        sort(piles.begin(), piles.end());
-        long left = 1, right = piles[n-1];
-        int res = piles[n-1];
+
+        long left = 1, right = *max_element(piles.begin(), piles.end());
+        int res = right;
 
         while(left <= right){
             long k = left + (right - left)/2;
 
-            long hours = 0;
+            long hoursTaken = 0;
+
             for(int i: piles){
-                hours += (i/k);
-                if(i%k != 0) hours++;
+                hoursTaken += (i/k);
+                if(i%k != 0) hoursTaken++;
             }
 
-            if(hours <= h){
+            if(hoursTaken <= h){
                 if(res >= k){
                     res = k;
                 }
-                right = k - 1;
+                right = k-1;
             }else{
-                left = k + 1;
+                left = k+1;
             }
+
         }
         return res;
     }
 };
-
-//[30,11,23,4,20], h = 6
-
-//[30, 23, 20, 11, 4]
