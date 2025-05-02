66e89bf618479d28d78f95b19569f9e14340f30a
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-02 20:20:49 +0530

    C++ Solution
          Submission Statistics:
          Question #: 907
          Language: C++
          Runtime: 4 ms
          Runtime percentile: 90.24
          Memory: 22.772 MB
          Memory percentile: 96.83

--- 907. Koko Eating Bananas/Solution.cpp
+++ 907. Koko Eating Bananas/Solution.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
-    long helper(int k, vector<int>&piles){
-        long hours = 0;
-        for(int i:piles){
-            hours += ceil((double)i/(double)k);
-            //if(i%k != 0) hours++;
-        }
-        return hours;
-
-    }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int low = 1;
-        int high = *max_element(piles.begin(), piles.end());
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            long k = helper(mid, piles);
-            if(k <= h){
-                high = mid-1;
+        int n = piles.size();
+        sort(piles.begin(), piles.end());
+        long left = 1, right = piles[n-1];
+
+        while(left <= right){
+            long mid = left + (right - left)/2;
+
+            long x = 0;
+            for(int i: piles){
+                x += (i/mid);
+                if(i%mid != 0) x++;
+            }
+            if(x <= h){
+                right = mid - 1;
             }else{
-                low = mid+1;
+                left = mid + 1;
             }
         }
-        return low;
+        return left;
     }
-
-
-    /*
-    piles = [30,11,23,4,20]
-    
-    [4, 11, 20, 23, 30] -> 88
-
-    1 - 88
-
-    44 
-
-    h = 5
-    
-    */
 };
+
+//[30,11,23,4,20], h = 6
+
+//[30, 23, 20, 11, 4]
