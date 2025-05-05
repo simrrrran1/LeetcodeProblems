5e8cf630f1967e2f3c250dc965033a1fdf2b9253
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-05 17:22:25 +0530

    C++ Solution
          Submission Statistics:
          Question #: 23
          Language: C++
          Runtime: 112 ms
          Runtime percentile: 10.30
          Memory: 18.344 MB
          Memory percentile: 81.02

--- 23. Merge k Sorted Lists/Solution.cpp
+++ 23. Merge k Sorted Lists/Solution.cpp
@@ -1,23 +1,47 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<int,vector<int>,greater<int>>pq;
-        ListNode* newHead=new ListNode(0);
-        for(int i=0;i<lists.size();i++){
-            ListNode* head=lists[i];
-            while(head){
-                pq.push(head->val);
-                head=head->next;
+    ListNode* merge(ListNode* a, ListNode* b){
+        if(!a) return b;
+        if(!b) return a;
+        ListNode* dummy = new ListNode(0);
+        ListNode* temp = dummy;
+
+        while(a && b){
+            if(a->val < b->val){
+                temp->next = a;
+                a = a->next;
+            }else{
+                temp->next = b;
+                b = b->next;
             }
+            temp = temp->next;
         }
-        ListNode* temp=newHead;
-        while(!pq.empty()){
-            ListNode* temp1=new ListNode(pq.top());
-            pq.pop();
-            temp->next=temp1;
-            temp=temp->next;
+        if(a){
+            temp->next = a;
         }
-        return newHead->next;
+        if(b){
+            temp->next = b;
+        }
+
+        return dummy->next;
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.empty()) return NULL;
+        if(lists.size() == 1) return lists[0];
+        ListNode* head = merge(lists[0], lists[1]);
+        for(int i=2; i<lists.size(); i++){
+            head = merge(head, lists[i]);
+        }
+        return head;
     }
 };
