761bf6f579cf4331ee6c8f4613452f1cefa4377d
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-07 23:43:38 +0530

    C++ Solution
          Submission Statistics:
          Question #: 658
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 35.672 MB
          Memory percentile: 98.93

---
+++ 658. Find K Closest Elements/Solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int n = arr.size();
+        int left = 0, right = n-k;
+
+        while(left < right){
+            int mid = left + (right-left)/2;
+
+            if(x-arr[mid] > arr[mid+k]-x){
+                left = mid+1;
+            }else{
+                right = mid;
+            }
+        }
+        return vector<int> (arr.begin() + left , arr.begin() + left + k); 
+    }
+};
