59c48f37250183820a38188a3c595769d2e14e0d
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-16 13:46:39 +0530

    C++ Solution
          Submission Statistics:
          Question #: 235
          Language: C++
          Runtime: 31 ms
          Runtime percentile: 6.84
          Memory: 23.588 MB
          Memory percentile: 33.28

--- 235. Lowest Common Ancestor of a Binary Search Tree/Solution.cpp
+++ 235. Lowest Common Ancestor of a Binary Search Tree/Solution.cpp
@@ -11,15 +11,10 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root->val==p->val || root->val==q->val)return root;
-        else if(root->val>=p->val && root->val>=q->val){
-            return lowestCommonAncestor(root->left,p,q);
-        }else if(root->val<p->val && root->val<q->val){
-            return lowestCommonAncestor(root->right,p,q);
-        }else if(root->val>=p->val && root->val<q->val){
-            return root;
-        }else if(root->val<=q->val && root->val>p->val){
-            return root;
+        if(root){
+            if(p->val <= root->val && root->val <= q->val) return root;
+            else if(root->val < p->val && root->val < q->val) return lowestCommonAncestor(root->right, p, q);
+            else if(p->val < root->val && q->val < root->val) return lowestCommonAncestor(root->left, p, q);
         }
         return root;
     }
