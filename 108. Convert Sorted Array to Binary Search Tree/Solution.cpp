1700e682e723cc79b06fdef33445083f55a00688
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-16 23:12:42 +0530

    C++ Solution
          Submission Statistics:
          Question #: 108
          Language: C++
          Runtime: 3 ms
          Runtime percentile: 60.48
          Memory: 22.968 MB
          Memory percentile: 55.74

--- 108. Convert Sorted Array to Binary Search Tree/Solution.cpp
+++ 108. Convert Sorted Array to Binary Search Tree/Solution.cpp
@@ -11,15 +11,19 @@
  */
 class Solution {
 public:
-    TreeNode* dfs(vector<int>& nums, int l, int r){
-        if(l > r) return NULL;
-        int mid = (l+r) / 2;
+    TreeNode* helper(vector<int>& nums, int left, int right){
+        if(left > right){
+            return NULL;
+        }
+
+        int mid = left + (right-left)/2;
         TreeNode* root = new TreeNode(nums[mid]);
-        root->left = dfs(nums, l, mid-1);
-        root->right = dfs(nums, mid+1, r);
+        root->left = helper(nums, left, mid-1);
+        root->right = helper(nums, mid+1, right);
         return root;
     }
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        return dfs(nums, 0, nums.size()-1);
+        int n = nums.size();
+        return helper(nums, 0, n-1);
     }
 };
