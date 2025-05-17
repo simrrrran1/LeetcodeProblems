a9de67ef216f497ab42acad34dc439f65cebd5d2
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-17 22:33:27 +0530

    C++ Solution
          Submission Statistics:
          Question #: 134
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 112.348 MB
          Memory percentile: 71.65

--- 134. Gas Station/Solution.cpp
+++ 134. Gas Station/Solution.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int n=gas.size();
-        int gSum=0,cSum=0;
-        for(int i=0;i<n;i++){
-            gSum+=gas[i];
-            cSum+=cost[i];
+        int sumGas = 0;
+        int sumCost = 0;
+        int n = gas.size();
+        for(int i=0; i<n; i++){
+            sumGas += gas[i];
+            sumCost += cost[i];
         }
-        if(gSum<cSum)return -1;
-        int sum=0,ans=0;
-        for(int i=0;i<n;i++){
-            sum+=(gas[i]-cost[i]);
-            if(sum<0){
-                sum=0;
-                ans=i+1;
+        if(sumCost > sumGas) return -1;
+
+        int ans = 0;
+        int sum = 0;
+
+        for(int i=0; i<n; i++){
+            sum += (gas[i] - cost[i]);
+            if(sum < 0){
+                sum = 0;
+                ans = i+1;
             }
         }
         return ans;
+
+
+
+
     }
 };
+
+
+//gas =   [1,   2,  3,  4, 5] 
+//cost =  [3,   4,  5,  1, 2]
+//total = [-2, -2, -2,  3, 3]
