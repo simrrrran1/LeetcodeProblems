d4f1c3c7aa1907cb8a459dbf36cc6ae1fdf6df66
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-19 16:56:43 +0530

    C++ Solution
          Submission Statistics:
          Question #: 778
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 8.516 MB
          Memory percentile: 25.62

--- 778. Reorganize String/Solution.cpp
+++ 778. Reorganize String/Solution.cpp
@@ -1,59 +1,36 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-
-        unordered_map<char, int> charCount;
-
-        for (int i = 0; i < s.length(); i++) {
-            char chr = s[i];
-            charCount[chr]++; // counts the frequency of each character
+        unordered_map<char, int> mp;
+        for(char c: s){
+            mp[c]++;
         }
 
-        int maxFreq = 0;  //intialise max freq to 0
-        char maxChar = '\0';  //intialise an empty character
-                             
+        priority_queue<pair<int,char>> pq;
+        for(auto i: mp){
+            pq.push({i.second, i.first});
+        }
 
-        for (const auto& pair : charCount) {
-            if (pair.second > maxFreq) {
-                maxFreq = pair.second; // to find the maximum frequency
-                maxChar = pair.first;  //gives character having maximum frequency
+        pair<int, char> prev = {0, '#'};
+        string res = "";
+
+        while(!pq.empty() || (prev.first > 0)){
+            if(prev.first > 0 && pq.empty()) return ""; // No valid reorg
+
+            pair<int, char> p = pq.top(); pq.pop(); // FIXED: pop the top
+            res += p.second;
+            p.first--;
+
+            if(prev.first > 0) {
+                pq.push(prev);
+            }
+
+            if(p.first > 0){
+                prev = p;
+            } else {
+                prev = {0, '#'};
             }
         }
-
-        int indx = 0; //start filling ; initialise a starting index
-
-        while (maxFreq > 0 && indx < s.length()) {  // Step 1: Place the max frequency character at even indices
-                                    
-            s[indx] = maxChar;
-            maxFreq--; //reduce it frequency
-            indx += 2;  //even index
-        }
-
-        if (maxFreq !=0) {   // if we cant place first character of maximum frequency correctly then we cant arrange all other properly
-            return ""; //therefore return empty string
-        }
-
-        // Step 2: Place the rest of the characters
-        for (const auto& pair : charCount) {
-            char c = pair.first;
-            int count = pair.second;
-
-            if (c == maxChar) continue;
-
-            while (count > 0) {
-                {
-                    if (indx >= s.length()) // if indices are full 
-                        indx = 1;  // start filling at odd indices if even 
-                }
-
-                s[indx] = c;
-                count--;
-                indx += 2;
-            }
-        }
-        return s;
+        return res;
     }
 };
-
-// T.C = O(N)
-// s.c = O(1)
