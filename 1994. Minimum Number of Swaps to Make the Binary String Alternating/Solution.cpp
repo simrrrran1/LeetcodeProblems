f25d8e2951c2aaaf8792b280c1256cf61761d82f
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-19 19:39:40 +0530

    C++ Solution
          Submission Statistics:
          Question #: 1994
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 9.168 MB
          Memory percentile: 96.76

--- 1994. Minimum Number of Swaps to Make the Binary String Alternating/Solution.cpp
+++ 1994. Minimum Number of Swaps to Make the Binary String Alternating/Solution.cpp
@@ -1,35 +1,13 @@
 class Solution {
 public:
     int minSwaps(string s) {
-        int z=0,o=0;
-        int n=s.length();
-        for(int i=0;i<n;i++){
-            if(s[i]=='0')z++;
-            else o++;
-        }
-        if(abs(z-o)>1) return -1;
-        string s1="",s2="";
-        for(int i=0;i<n;i++){
-            if(i%2==0){
-                s1+='0';
-                s2+='1';
-            }
-            else{
-                s1+='1';
-                s2+='0';
-            }
-        }
-        int c1=0,c2=0;
-        if(z==o){
-            for(int i=0;i<n;i++){
-                if(s[i]!=s1[i]) c1++;
-                if(s[i]!=s2[i]) c2++;
-            }
-            return min(c1/2, c2/2);
-        }
-        if(o>z) s1=s2;
-        for(int i=0;i<n;i++)
-            if(s[i]!=s1[i]) c1++;
-        return c1/2;
+    int cnt0 = count(begin(s), end(s), '0'), cnt1 = s.size() - cnt0, miss0 = 0, miss1 = 0;
+    if (abs(cnt0 - cnt1) > 1)
+        return -1;
+    for (int i = 0; i < s.size(); i += 2) {
+        miss0 += s[i] != '0';
+        miss1 += s[i] != '1';
     }
+    return cnt0 == cnt1 ? min(miss0, miss1) : cnt0 > cnt1 ? miss0 : miss1;
+}
 };
