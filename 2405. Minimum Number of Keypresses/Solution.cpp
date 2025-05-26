e64bc72a57dc590f83159a557c3c836f154ede42
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-27 00:09:04 +0530

    C++ Solution
          Submission Statistics:
          Question #: 2405
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 11.66 MB
          Memory percentile: 63.78

--- 2405. Minimum Number of Keypresses/Solution.cpp
+++ 2405. Minimum Number of Keypresses/Solution.cpp
@@ -2,26 +2,21 @@ class Solution {
 public:
     int minimumKeypresses(string s) {
         int n = s.size();
-        vector<int> v(26, 0);
-        for(char c: s){
-            v[c - 'a']++;
-        }
-        sort(v.begin(), v.end(), greater<int>());
+        int ans = 0;
 
-        int ans = 0, count = 0;
+        vector<int> v(26, 0);
+        for(int i=0; i<n; i++){
+            v[s[i] - 'a']++;
+        }
+
+        sort(v.begin(), v.end(), greater<int>());
+        int count = 0;
 
         for(int i=0; i<26; i++){
-            if(i % 9 == 0) count += 1;
-            ans += (count * v[i]);
+            if(i % 9 == 0) count++;
+            int freq = v[i];
+            ans += (freq * count);
         }
-
         return ans;
-
-        
     }
 };
-
-//abcdefghijkl
-
-//apple
-//abcdefghi
