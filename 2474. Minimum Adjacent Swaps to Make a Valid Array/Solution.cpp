1eb6bf1fd3927557f011f7fa84c2e20ca7444838
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-26 23:53:19 +0530

    C++ Solution
          Submission Statistics:
          Question #: 2474
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 53.952 MB
          Memory percentile: 5.84

--- 2474. Minimum Adjacent Swaps to Make a Valid Array/Solution.cpp
+++ 2474. Minimum Adjacent Swaps to Make a Valid Array/Solution.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int minimumSwaps(vector<int>& nums) {
-        int largest = nums[0], smallest = nums[0], smallest_idx = 0, largest_idx = 0;
         int n = nums.size();
+        int largest = nums[0], smallest = nums[0], largest_idx = 0, smallest_idx = 0;
 
         for(int i=0; i<n; i++){
-            if(nums[i] < smallest){
-                smallest = nums[i];
-                smallest_idx = i;
-            }
-            if(nums[i] >= largest){
+            if(largest <= nums[i]){
                 largest = nums[i];
                 largest_idx = i;
             }
+            if(smallest > nums[i]){
+                smallest = nums[i];
+                smallest_idx = i;
+            }
         }
 
-        int swaps = smallest_idx + (n - 1 - largest_idx);
-        return smallest_idx > largest_idx ? swaps - 1 : swaps; 
+        int dis = smallest_idx + (n-1-largest_idx);
+
+        return smallest_idx > largest_idx ? dis-1 : dis;
     }
 };
