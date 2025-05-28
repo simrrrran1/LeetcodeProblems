46d125357643bc98f4c2f07548f5227aa990cabf
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-29 01:27:59 +0530

    C++ Solution
          Submission Statistics:
          Question #: 14
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 11.924 MB
          Memory percentile: 49.87

--- 14. Longest Common Prefix/Solution.cpp
+++ 14. Longest Common Prefix/Solution.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        sort(strs.begin(), strs.end());
-
-        string ans = "";
         int n = strs.size();
-
+        sort(strs.begin(), strs.end());
         string first = strs[0];
         string last = strs[n-1];
+        string ans = "";
 
-        for(int i=0; i<first.size(); i++){
+        for(int i=0; i < first.size(); i++){
             if(first[i] != last[i]) return ans;
             ans += first[i];
         }
-
         return ans;
     }
 };
