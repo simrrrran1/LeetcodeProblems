e4feb13ba6c88f3830219a0075d45bf92d958787
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-29 01:32:06 +0530

    C++ Solution
          Submission Statistics:
          Question #: 20
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 8.812 MB
          Memory percentile: 34.92

--- 20. Valid Parentheses/Solution.cpp
+++ 20. Valid Parentheses/Solution.cpp
@@ -2,37 +2,26 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> s1;
-        char c;
-        for(int i=0; i<s.size(); i++){
-            if(s[i] == '{' || s[i] == '(' || s[i] == '['){
-                s1.push(s[i]);
-                continue;
-            }
-            else if(s[i] == ')' || s[i] == '}' || s[i] == ']'){
+        int n = s.size();
+
+        for(int i=0; i<n; i++){
+            if(s[i] == '}' || s[i] == ']' || s[i] == ')'){
                 if(s1.empty()) return false;
-                switch(s[i]){
-                    
-                    case '}':
-                        c = s1.top();
-                        if(c == '[' || c == '(') return false;
-                        s1.pop();
-                        break;
-
-                    case  ']':
-                        c = s1.top();
-                        if(c == '{' || c == '(') return false;
-                        s1.pop();
-                        break;
-
-                    case ')':
-                        c = s1.top();
-                        if(c == '[' || c == '{') return false;
-                        s1.pop();
-                        break;
+                else{
+                    char c = s1.top();
+                    s1.pop();
+                    if(c == '('){
+                        if(s[i] == '}' || s[i] == ']') return false;
+                    }else if(c == '{'){
+                        if(s[i] == ')' || s[i] == ']') return false;
+                    }else if(c == '['){
+                        if(s[i] == '}' || s[i] == ')') return false;
+                    }
                 }
+            }else{
+                s1.push(s[i]);
             }
         }
-
         return s1.empty();
     }
 };
