597aae98be27c0fb34ce7e7683439659284aa996
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-29 01:35:15 +0530

    C++ Solution
          Submission Statistics:
          Question #: 21
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 19.372 MB
          Memory percentile: 86.46

--- 21. Merge Two Sorted Lists/Solution.cpp
+++ 21. Merge Two Sorted Lists/Solution.cpp
@@ -11,25 +11,25 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL && list2==NULL)return NULL;
-        if(list1==NULL)return list2;
-        if(list2==NULL)return list1;
-        ListNode* head=new ListNode(0);
-        ListNode* temp=head;
+        ListNode* head = new ListNode(0);
+        ListNode* temp = head;
+
         while(list1 && list2){
-            if(list1->val < list2-> val){
-                ListNode* temp1=new ListNode(list1->val);
-                temp->next=temp1;
-                list1=list1->next;
+            if(list1->val < list2->val){
+                temp->next = list1;
+                list1 = list1->next;
             }else{
-                ListNode* temp1=new ListNode(list2->val);
-                temp->next=temp1;
-                list2=list2->next;
+                temp->next = list2;
+                list2 = list2->next;
             }
-            temp=temp->next;
+            temp = temp->next;
         }
-        if(list1==NULL)temp->next=list2;
-        if(list2==NULL)temp->next=list1;
+        if(!list1){
+            temp->next = list2;
+        }
+        if(!list2){
+            temp->next = list1;
+        }
         return head->next;
     }
 };
