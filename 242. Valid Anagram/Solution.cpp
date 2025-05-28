586572144b587ddfcd0ded2bbd7cf9d429a7d75f
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-29 02:33:20 +0530

    C++ Solution
          Submission Statistics:
          Question #: 242
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 9.712 MB
          Memory percentile: 48.17

--- 242. Valid Anagram/Solution.cpp
+++ 242. Valid Anagram/Solution.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size())return false;
-        //sort(s.begin(),s.end());
-        //sort(t.begin(),t.end());
-        vector<int>v1(26,0),v2(26,0);
-        for(int i=0;i<s.size();i++){
-            v1[s[i]-'a']++;
-            v2[t[i]-'a']++;
+        vector<int> v(26, 0);
+        if(s.size() != t.size()) return false;
+        for(int i=0; i<s.size(); i++){
+            v[s[i] - 'a']++;
+            v[t[i] - 'a']--;
         }
-        return v1==v2;
+        for(int i=0; i<26; i++){
+            if(v[i] != 0) return false;
+        }
+        return true;
     }
 };
