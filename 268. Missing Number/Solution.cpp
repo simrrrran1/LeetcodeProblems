5e560f9e408377d5b37e2328160155072706dd07
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-29 02:35:15 +0530

    C++ Solution
          Submission Statistics:
          Question #: 268
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 21.74 MB
          Memory percentile: 69.78

--- 268. Missing Number/Solution.cpp
+++ 268. Missing Number/Solution.cpp
@@ -2,15 +2,9 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        
-        int sum = n*(n+1)/2;
-        int sum1 = 0;
-
-        for(int i=0; i<n; i++){
-            sum1 += (nums[i]);
-        }
-
-        return (sum - sum1);
-
+        int sum = 0;
+        for(int i: nums) sum += i;
+        int sum1 = n*(n+1)/2;
+        return abs(sum1 - sum);
     }
 };
