55b3abdad9b5b38ef31f74005f5138d8e8e63967
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-29 00:08:05 +0530

    C++ Solution
          Submission Statistics:
          Question #: 974
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 16.488 MB
          Memory percentile: 53.15

--- 974. Reorder Data in Log Files/Solution.cpp
+++ 974. Reorder Data in Log Files/Solution.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     vector<string> reorderLogFiles(vector<string>& logs) {
-        vector<string> letter_logs;
-        vector<string> digit_logs;
+        vector<string> letterLog;
+        vector<string> digitLog;
         vector<string> ans;
 
         for(int i=0; i<logs.size(); i++){
             int idx = logs[i].find(" ") + 1;
-            if(logs[i].at(idx) >= '0' && logs[i].at(idx) <= '9'){
-                digit_logs.push_back(logs[i]);
+            if(logs[i][idx] >= '0' && logs[i][idx] <= '9'){
+                digitLog.push_back(logs[i]);
             }else{
-                letter_logs.push_back(logs[i]);
+                letterLog.push_back(logs[i]);
             }
         }
 
-        sort(letter_logs.begin(), letter_logs.end(), [](const string&a , const string&b){
-            int space1 = a.find(" ") + 1;
-            int space2 = b.find(" ") + 1;
+        sort(letterLog.begin(), letterLog.end(), [](string& a, string& b){
+            int aIdx = a.find(" ") + 1;
+            int bIdx = b.find(" ") + 1;
 
-            string contentA = a.substr(space1);
-            string contentB = b.substr(space2);
+            string contentA = a.substr(aIdx);
+            string contentB = b.substr(bIdx);
 
             if(contentA == contentB){
                 return a < b;
@@ -27,18 +27,8 @@ public:
             return contentA < contentB;
         });
 
-        for(auto i: letter_logs){
-            ans.push_back(i);
-        }
-        for(int i=0; i<digit_logs.size(); i++){
-            ans.push_back(digit_logs[i]);
-        }
-        return ans;
-
-
-
-
-
+        for(int i=0; i<letterLog.size(); i++) ans.push_back(letterLog[i]);
+        for(int i=0; i<digitLog.size(); i++) ans.push_back(digitLog[i]);
 
         return ans;
     }
