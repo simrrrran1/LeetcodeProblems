f5e2d45ce7f01a2b7d7e0367326eb6cd09a8ddaf
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-29 01:24:08 +0530

    C++ Solution
          Submission Statistics:
          Question #: 13
          Language: C++
          Runtime: 6 ms
          Runtime percentile: 53.90
          Memory: 13.532 MB
          Memory percentile: 9.14

--- 13. Roman to Integer/Solution.cpp
+++ 13. Roman to Integer/Solution.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char,int>mp;
-        mp.insert({'I',1});
-        mp.insert({'V',5});
-        mp.insert({'X',10});
-        mp.insert({'L',50});
-        mp.insert({'C',100});
-        mp.insert({'D',500});
-        mp.insert({'M',1000});
-        int ans=0;
-        for(int i=0;i<s.size();i++){
-            if(i+1<s.size() && mp[s[i]]<mp[s[i+1]])ans-=mp[s[i]];
-            else ans+=mp[s[i]];
+        unordered_map<char, int> mp = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+
+        int ans = 0;
+
+        for(int i=0; i<s.size(); i++){
+            if(i < s.size() && mp[s[i]] < mp[s[i+1]]) ans -= mp[s[i]];
+            else ans += mp[s[i]];
         }
         return ans;
     }
