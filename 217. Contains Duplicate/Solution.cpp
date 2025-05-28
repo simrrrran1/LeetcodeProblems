7d95bea22cbf70aabc539a7b58e4cef91a9a5272
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-29 02:02:25 +0530

    C++ Solution
          Submission Statistics:
          Question #: 217
          Language: C++
          Runtime: 51 ms
          Runtime percentile: 47.88
          Memory: 90.952 MB
          Memory percentile: 22.88

--- 217. Contains Duplicate/Solution.cpp
+++ 217. Contains Duplicate/Solution.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int n=nums.size();
-        unordered_map<int,int> mp;
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-            if(mp[nums[i]]>1)return true;
+        unordered_map<int, int> mp;
+        for(int i: nums){
+            if(mp.find(i) != mp.end()) return true;
+            mp[i]++;
         }
         return false;
     }
