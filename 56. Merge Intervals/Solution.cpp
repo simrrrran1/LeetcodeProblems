6a338700b0d300c3d7cc1288c75e69eb497ed30f
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-30 03:02:21 +0530

    C++ Solution
          Submission Statistics:
          Question #: 56
          Language: C++
          Runtime: 5 ms
          Runtime percentile: 56.63
          Memory: 23.836 MB
          Memory percentile: 59.31

--- 56. Merge Intervals/Solution.cpp
+++ 56. Merge Intervals/Solution.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> ans;
-        int n = intervals.size();
         sort(intervals.begin(), intervals.end());
-
         pair<int,int> p = {intervals[0][0], intervals[0][1]};
-
-        for(int i=1; i<n; i++){
+        for(int i=0; i<intervals.size(); i++){
             if(p.second < intervals[i][0]){
                 ans.push_back({p.first, p.second});
                 p.first = intervals[i][0];
@@ -18,5 +15,6 @@ public:
         }
         ans.push_back({p.first, p.second});
         return ans;
+
     }
 };
