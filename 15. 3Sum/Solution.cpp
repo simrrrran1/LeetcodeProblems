8312d4e8713c861e25d67b1c402010bf72478350
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-30 00:29:17 +0530

    C++ Solution
          Submission Statistics:
          Question #: 15
          Language: C++
          Runtime: 39 ms
          Runtime percentile: 95.39
          Memory: 29.108 MB
          Memory percentile: 52.20

--- 15. 3Sum/Solution.cpp
+++ 15. 3Sum/Solution.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
 
-        sort(nums.begin(), nums.end());
-
         for(int i=0; i<nums.size()-2; i++){
-            if(i>0 && nums[i] == nums[i-1]){
+            if(i > 0 && nums[i-1] == nums[i]){
                 continue;
-            }
+            } 
             int left = i+1, right = nums.size()-1;
-
             while(left < right){
-                int sum = nums[left]+nums[right]+nums[i];
-                
-                if(sum==0){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0){
                     ans.push_back({nums[i], nums[left], nums[right]});
-                    while(left<right && nums[left] == nums[++left]);
-                    while(left<right && nums[right] == nums[--right]);
-
+                    while(left < right && nums[left] == nums[++left]);
+                    while(left < right && nums[right] == nums[--right]);
+                    
                 }else if(sum < 0){
                     left++;
-                }else{
+                }else {
                     right--;
                 }
             }
+
         }
+
+
 
         return ans;
     }
