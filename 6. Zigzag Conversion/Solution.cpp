55ce1da949cecf205b628d438791272267db9be7
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-29 20:36:21 +0530

    C++ Solution
          Submission Statistics:
          Question #: 6
          Language: C++
          Runtime: 9 ms
          Runtime percentile: 36.26
          Memory: 13.944 MB
          Memory percentile: 66.56

--- 6. Zigzag Conversion/Solution.cpp
+++ 6. Zigzag Conversion/Solution.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        int n=s.size();
-        if(numRows==1)return s;
-        int row=0;
-        bool dir=true;
-        string ans="";
-        vector<string> v(numRows,"");
-        for(int i=0;i<n;i++){
+        if(numRows == 1) return s;
+        vector<string> v(numRows);
+        int i = 0;
+        int row = 0, dir = 1;
+        while(i<s.size()){
             v[row].push_back(s[i]);
-            if(row==numRows-1)dir=false;
-            else if(row==0)dir=true;
-            if(dir==true)row++;
+            if(row == numRows-1) dir = -1;
+            else if(row == 0) dir = 1;
+            if(dir == 1) row++;
             else row--;
+            i++;
         }
-        for(int i=0;i<numRows;i++)ans+=v[i];
+        string ans = "";
+        for(int i=0; i<numRows; i++){
+            ans += v[i];
+        }
         return ans;
     }
 };
