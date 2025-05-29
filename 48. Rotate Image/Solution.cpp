ba7851869a7b0d34705a2ce2871f3faff5fe0f81
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-30 02:41:01 +0530

    C++ Solution
          Submission Statistics:
          Question #: 48
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 10.264 MB
          Memory percentile: 34.42

--- 48. Rotate Image/Solution.cpp
+++ 48. Rotate Image/Solution.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        for(int i=0; i < matrix.size(); i++){
-            for(int j=0; j < i; j++){
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<i; j++){
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
 
-        for(int i=0; i < matrix.size(); i++){
+        for(int i=0; i<row; i++){
             reverse(matrix[i].begin(), matrix[i].end());
         }
+
     }
 };
