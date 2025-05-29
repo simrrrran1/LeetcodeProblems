2cec0fa3f8e372df0c50cc1550fb73c94c7ac4ad
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-29 20:23:33 +0530

    C++ Solution
          Submission Statistics:
          Question #: 5
          Language: C++
          Runtime: 20 ms
          Runtime percentile: 57.51
          Memory: 22.036 MB
          Memory percentile: 45.79

--- 5. Longest Palindromic Substring/Solution.cpp
+++ 5. Longest Palindromic Substring/Solution.cpp
@@ -2,34 +2,35 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n = s.size();
-        string res = "";
         int len = 0;
+        string ans;
+        for(int i=0; i<n; i++){
 
-        for(int i = 0; i < n; i++) {
+            // odd length palindrome
             int left = i, right = i;
-            // Odd length palindrome
-            while(left >= 0 && right < n && s[left] == s[right]) {
-                if((right - left + 1) > len) {
-                    res = s.substr(left, right - left + 1);
+
+            while(left >= 0 && right < n && s[left] == s[right]){
+                if(right - left + 1 > len){
                     len = right - left + 1;
+                    ans = s.substr(left, right-left+1);
                 }
                 left--;
                 right++;
             }
 
-            left = i;
-            right = i + 1;
-            // Even length palindrome
-            while(left >= 0 && right < n && s[left] == s[right]) {
-                if((right - left + 1) > len) {
-                    res = s.substr(left, right - left + 1);
+            // even length palindrome
+            left = i, right = i+1;
+
+            while(left >= 0 && right < n && s[left] == s[right]){
+                if(right - left + 1 > len){
                     len = right - left + 1;
+                    ans = s.substr(left, right-left+1);
                 }
                 left--;
                 right++;
             }
+
         }
-
-        return res;
+        return ans;
     }
 };
