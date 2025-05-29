a14ffc86150dfc3643958c5b7fc7a5ffd3b20fb7
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-29 23:23:56 +0530

    C++ Solution
          Submission Statistics:
          Question #: 7
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 8.584 MB
          Memory percentile: 53.85

--- 7. Reverse Integer/Solution.cpp
+++ 7. Reverse Integer/Solution.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int reverse(int x) {
-        long long ans=0;
+        long long ans = 0;
         while(x){
-            ans=(ans*10)+(x%10);
+            ans = ans*10 + (x%10);
             x/=10;
         }
-        return ans>INT_MAX||ans<INT_MIN?0:ans;
+        return ans > INT_MAX || ans < INT_MIN ? 0 : (int)ans;
+
     }
 };
