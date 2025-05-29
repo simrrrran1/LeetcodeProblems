6c68894b10fdd925979392fb95eb7dcb37f885ab
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-30 01:47:03 +0530

    C++ Solution
          Submission Statistics:
          Question #: 33
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 15.116 MB
          Memory percentile: 66.35

--- 33. Search in Rotated Sorted Array/Solution.cpp
+++ 33. Search in Rotated Sorted Array/Solution.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0, right = nums.size()-1;
+        int n = nums.size();
+        int left = 0, right = n-1;
 
         while(left <= right){
             int mid = left + (right-left)/2;
