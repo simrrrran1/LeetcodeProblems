165f90a872fc210d29bc3eecfd2eea2500ca80ca
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-30 01:52:54 +0530

    C++ Solution
          Submission Statistics:
          Question #: 34
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 17.588 MB
          Memory percentile: 52.35

--- 34. Find First and Last Position of Element in Sorted Array/Solution.cpp
+++ 34. Find First and Last Position of Element in Sorted Array/Solution.cpp
@@ -3,27 +3,24 @@ public:
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> v(2, -1);
         int n = nums.size();
-        int left = 0, right = n-1;
-
+        int left = 0, right = n - 1;
         while(left <= right){
-            int mid = left + ((right-left)/2);
-
-            if(nums[mid] < target) left = mid+1;
-            else if(nums[mid] > target) right = mid-1;
-            else{
-                int x = mid-1;
-                while(x>=0 && nums[mid] == nums[x]){
-                    x--;
+            int mid = left + (right-left)/2;
+            if(nums[mid] == target){
+                int c1 = mid - 1;
+                while(c1 >=0 && nums[c1] == nums[mid]){
+                    c1--;
                 }
-                v[0] = x+1;
-                int y = mid+1;
-                while(y<n && nums[mid] == nums[y]){
-                    y++;
+                int c2 = mid + 1;
+                while(c2 < n && nums[c2] == nums[mid]){
+                    c2++;
                 }
-                v[1] = y-1;
+                v[0] = c1+1;
+                v[1] = c2-1;
                 break;
-            }
-        }
+            }else if(nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        } 
         return v;
     }
 };
