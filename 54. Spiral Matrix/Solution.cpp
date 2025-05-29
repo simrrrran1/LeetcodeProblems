606b621d48a71ec3574c8036ed3095257e1d65eb
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-30 02:52:31 +0530

    C++ Solution
          Submission Statistics:
          Question #: 54
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 9.252 MB
          Memory percentile: 78.92

--- 54. Spiral Matrix/Solution.cpp
+++ 54. Spiral Matrix/Solution.cpp
@@ -1,27 +1,48 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int row=matrix.size();
-        int col=matrix[0].size();
-        int up=0,down=row-1,left=0,right=col-1;
-        int dir=0;
-        vector<int>v;
-        while(up<=down && left<=right){
-            if(dir==0){
-                for(int i=left;i<=right;i++)v.push_back(matrix[up][i]);
-                up++;
-            }else if(dir==1){
-                for(int i=up;i<=down;i++)v.push_back(matrix[i][right]);
-                right--;
-            }else if(dir==2){
-                for(int i=right;i>=left;i--)v.push_back(matrix[down][i]);
-                down--;
-            }else{
-                for(int i=down;i>=up;i--)v.push_back(matrix[i][left]);
-                left++;
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int up = 0, down = row - 1, left = 0, right = col - 1;
+        int dir = 0;
+        vector<int> v;
+
+        while(up <= down && left <= right){
+            switch(dir){
+                case 0:
+                    for(int i=left; i<=right; i++){
+                        v.push_back(matrix[up][i]);
+                    }
+                    up++;
+                    dir++;
+                    break;
+                case 1:
+                    for(int i=up; i<=down; i++){
+                        v.push_back(matrix[i][right]);
+                    }
+                    right--;
+                    dir++;
+                    break;
+                case 2:
+                    for(int i=right; i>=left; i--){
+                        v.push_back(matrix[down][i]);
+                    }
+                    down--;
+                    dir++;
+                    break;
+                case 3:
+                    for(int i=down; i>=up; i--){
+                        v.push_back(matrix[i][left]);
+                    }
+                    left++;
+                    dir++;
+                    break;
+
             }
-            dir=(dir+1)%4;
+            dir %= 4;
+
         }
         return v;
     }
+
 };
