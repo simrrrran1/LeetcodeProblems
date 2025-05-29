98ebb07b3496cd80c2d60fc40873a358fa7581b9
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-30 02:45:17 +0530

    C++ Solution
          Submission Statistics:
          Question #: 53
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 71.748 MB
          Memory percentile: 52.31

--- 53. Maximum Subarray/Solution.cpp
+++ 53. Maximum Subarray/Solution.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int ans = INT_MIN, sum = 0;
+        int sum = nums[0], curr = 0;
         for(int i=0; i<nums.size(); i++){
-            sum += nums[i];
-            ans = max(sum, ans);
-            if(sum < 0) sum = 0;
+            curr += nums[i];
+            sum = max(sum, curr);
+            if(curr < 0)curr = 0;
         }
-        return ans;
+        return sum;
     }
 };
