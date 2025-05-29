7d3990b553c9fdcdd09f27ed7e9356122123876f
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-30 02:36:19 +0530

    C++ Solution
          Submission Statistics:
          Question #: 45
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 20.356 MB
          Memory percentile: 84.49

--- 45. Jump Game II/Solution.cpp
+++ 45. Jump Game II/Solution.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int left = 0, right = 0, ans = 0;
+        int left = 0, right = 0; // The current range of indices we can reach with the current number of jumps
+        int ans = 0;             // Number of jumps made
 
-        while(right < nums.size()-1){
-            int farthest = 0;
-            for(int i=left; i<=right; i++){
-                farthest = max(farthest, i+nums[i]);
+        // Continue jumping until we've reached or passed the last index
+        while (right < nums.size() - 1) {
+            int farthest = 0;    // The farthest index we can reach in the next jump
+
+            // Explore all indices in the current range [left, right]
+            for (int i = left; i <= right; i++) {
+                // Update the farthest we can go from this position
+                farthest = max(farthest, i + nums[i]);
             }
-            left = right+1;
-            right = farthest;
-            ans += 1;
+
+            // Prepare for the next jump: update the window
+            left = right + 1;    // Move left to start of new range
+            right = farthest;   // Move right to the farthest reachable index in next jump
+
+            ans += 1;           // One more jump used
         }
 
-        return ans;
+        return ans;             // Total number of jumps needed
     }
 };
