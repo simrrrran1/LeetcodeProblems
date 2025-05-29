e60707aab07ae8111001a5fb6331bbe1a83ddbb7
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-30 00:47:12 +0530

    C++ Solution
          Submission Statistics:
          Question #: 22
          Language: C++
          Runtime: 4 ms
          Runtime percentile: 36.49
          Memory: 16.192 MB
          Memory percentile: 23.49

--- 22. Generate Parentheses/Solution.cpp
+++ 22. Generate Parentheses/Solution.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    
-    void helper(int n, string& s, vector<string>& ans, int open, int closed){
+    void fnc(int n, int open, int closed, string s, vector<string>& ans){
         if(open + closed == 2*n){
             ans.push_back(s);
             return;
         }
         if(open < n){
             s.push_back('(');
-            helper(n, s, ans, open+1, closed);
+            fnc(n, open+1, closed, s, ans);
             s.pop_back();
         }
         if(closed < open){
             s.push_back(')');
-            helper(n, s, ans, open, closed+1);
+            fnc(n, open, closed+1, s, ans);
             s.pop_back();
         }
-
-    }    
-
+    }
     vector<string> generateParenthesis(int n) {
-        string s = "";
         vector<string> ans;
-        helper(n, s, ans, 0, 0);
+        string s = "";
+        fnc(n, 0, 0, s, ans);
+
         return ans;
     }
 };
