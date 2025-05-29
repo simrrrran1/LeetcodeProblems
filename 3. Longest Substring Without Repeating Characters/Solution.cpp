c81a9c5f61579c514a644bb6ab7ec8abd767c000
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-29 20:15:50 +0530

    C++ Solution
          Submission Statistics:
          Question #: 3
          Language: C++
          Runtime: 12 ms
          Runtime percentile: 46.22
          Memory: 14.412 MB
          Memory percentile: 15.23

--- 3. Longest Substring Without Repeating Characters/Solution.cpp
+++ 3. Longest Substring Without Repeating Characters/Solution.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int ans = 0;
         unordered_set<char> s1;
-        int left = 0, right = 0;
+        int left = 0, right = 0, ans = 0;
         while(right < s.size()){
-            while(s1.find(s[right]) != s1.end()){
-                s1.erase(s[left]);
-                left += 1;
-            }
-            s1.insert(s[right]);
-            ans = max(ans, right-left+1);
-            right++;
-        }
 
+            while(!s1.empty() && s1.find(s[right]) != s1.end()){
+                s1.erase(s[left]);
+                left++;
+            }
+            ans = max(ans, right-left+1);
+            s1.insert(s[right]);
+            right++;
+            
+        }
         return ans;
+
     }
 };
