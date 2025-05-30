c07fa85365cc0d9c498b559003f5fcefc81db09a
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-30 23:26:19 +0530

    C++ Solution
          Submission Statistics:
          Question #: 73
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 18.5 MB
          Memory percentile: 95.34

--- 73. Set Matrix Zeroes/Solution.cpp
+++ 73. Set Matrix Zeroes/Solution.cpp
@@ -1,25 +1,49 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int row=matrix.size();
-        int col=matrix[0].size();
-        int col0=1;//to check whether dummy row array has a zero marked or not 
-        for(int i=0;i<row;i++){
-            if(matrix[i][0]==0)col0=0;
-            for(int j=1;j<col;j++){
-                if(matrix[i][j]==0)
-                    matrix[i][0]=matrix[0][j]=0;
-            }
+        int row = matrix.size();
+        int col = matrix[0].size();
+        bool firstColHasZero = false;
+        bool firstRowHasZero = false;
 
+        for (int i = 0; i < row; i++) {
+            if (matrix[i][0] == 0) {
+                firstColHasZero = true;
+                break;
+            }
         }
 
-        for(int i=row-1;i>=0;i--){
-            for(int j=col-1;j>=1;j--){
-                if(matrix[i][0]==0||matrix[0][j]==0)
-                    matrix[i][j]=0;
+        // Step 2: Check if first row has any zero
+        for (int j = 0; j < col; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowHasZero = true;
+                break;
             }
-            if(col0==0)matrix[i][0]=0;
         }
 
+        for(int i=1; i<row; i++){
+            for(int j=1; j<col; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i=1; i<row; i++){
+            for(int j=1; j<col; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+
+        if(firstColHasZero) {
+            for(int i=0; i<row; i++) matrix[i][0] = 0;
+        }
+        if(firstRowHasZero){
+            for(int i=0; i<col; i++) matrix[0][i] = 0;
+        }
     }
 };
