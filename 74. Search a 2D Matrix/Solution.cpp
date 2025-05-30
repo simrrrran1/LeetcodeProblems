fefcf3377507c8b5eebdf1883a700888d544030d
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-30 23:30:59 +0530

    C++ Solution
          Submission Statistics:
          Question #: 74
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 13.42 MB
          Memory percentile: 7.47

--- 74. Search a 2D Matrix/Solution.cpp
+++ 74. Search a 2D Matrix/Solution.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int row = matrix.size();
-        int col = matrix[0].size();
+        int row = matrix.size();        // Number of rows
+        int col = matrix[0].size();     // Number of columns
 
-        int i = 0, j = col-1;
+        int i = 0, j = col - 1;         // Start from the top-right corner
 
+        // Loop until we are out of matrix bounds
         while(i < row && j >= 0){
-            if(matrix[i][j] > target) j--;
-            else if(matrix[i][j] < target) i++;
-            else return true;
+            if(matrix[i][j] > target) {
+                // If current element is greater than target, move left
+                j--;
+            }
+            else if(matrix[i][j] < target) {
+                // If current element is smaller than target, move down
+                i++;
+            }
+            else {
+                // Found the target
+                return true;
+            }
         }
+
+        // Target not found
         return false;
     }
 };
