cd3ce19bc92248aa7283b0ea9f27ea59c078b933
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-31 00:26:54 +0530

    C++ Solution
          Submission Statistics:
          Question #: 122
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 17.144 MB
          Memory percentile: 64.42

--- 122. Best Time to Buy and Sell Stock II/Solution.cpp
+++ 122. Best Time to Buy and Sell Stock II/Solution.cpp
@@ -3,11 +3,15 @@ public:
     int maxProfit(vector<int>& prices) {
         int ans = 0;
 
-        for(int i=1; i<prices.size(); i++){
-            if(prices[i-1] < prices[i])
-            ans += (prices[i] - prices[i-1]);
+        // Iterate over prices starting from day 1
+        for(int i = 1; i < prices.size(); i++) {
+            // If price increases from previous day, we consider it a profit opportunity
+            if(prices[i - 1] < prices[i])
+                ans += (prices[i] - prices[i - 1]);
+                // We add the profit made by buying on day i-1 and selling on day i
         }
+
+        // Return total accumulated profit
         return ans;
-        
     }
 };
