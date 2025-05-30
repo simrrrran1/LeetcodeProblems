18e5c13d31e408821db13e40717e5df35455ad1f
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-05-30 23:34:39 +0530

    C++ Solution
          Submission Statistics:
          Question #: 77
          Language: C++
          Runtime: 38 ms
          Runtime percentile: 61.72
          Memory: 62.252 MB
          Memory percentile: 99.04

--- 77. Combinations/Solution.cpp
+++ 77. Combinations/Solution.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    void helper(int n, int start, int k, vector<vector<int>>& ans, vector<int> v){
+    void fnc(int n, int start, int k, vector<int>& v, vector<vector<int>>& ans){
         if(v.size() == k){
             ans.push_back(v);
             return;
         }
+
         for(int i=start; i<=n; i++){
             v.push_back(i);
-            helper(n, i+1, k, ans, v);
+            fnc(n, i+1, k, v, ans);
             v.pop_back();
         }
+        
     }
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> ans;
         vector<int> v;
-        helper(n, 1, k, ans, v);
+
+        fnc(n, 1, k, v, ans);
         return ans;
     }
 };
