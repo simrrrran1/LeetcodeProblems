0cbce00c66a57958d7c87ae730f802fd8d1bbe27
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-06-06 01:31:54 +0530

    C++ Solution
          Submission Statistics:
          Question #: 210
          Language: C++
          Runtime: 6 ms
          Runtime percentile: 40.34
          Memory: 19.22 MB
          Memory percentile: 14.35

--- 210. Course Schedule II/Solution.cpp
+++ 210. Course Schedule II/Solution.cpp
@@ -1,47 +1,43 @@
 class Solution {
 public:
     bool dfs(vector<vector<int>>& adj, int start, vector<bool>& vis, stack<int>& s, unordered_set<int>& s1){
-        if(vis[start]) return false;
-        vis[start] =true;
         s1.insert(start);
+        vis[start] = true;
 
         for(auto& i: adj[start]){
             if(!vis[i]){
                 if(!dfs(adj, i, vis, s, s1)){
                     return false;
                 }
-            }else if(s1.find(i) != s1.end()) return false;
+            }else if(s1.count(i)) return false;
         }
         s1.erase(start);
         s.push(start);
         return true;
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> ans;
-
         vector<vector<int>> adj(numCourses);
-
         for(int i=0; i<prerequisites.size(); i++){
             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
 
-        stack<int> s;
         vector<bool> vis(numCourses, false);
+        stack<int> s;
 
         for(int i=0; i<numCourses; i++){
             if(!vis[i]){
-                unordered_set<int> s1; // to remember current path
+                unordered_set<int> s1;
                 if(!dfs(adj, i, vis, s, s1)){
                     return {};
                 }
             }
         }
 
+        vector<int> v;
         while(!s.empty()){
-            ans.push_back(s.top());
+            v.push_back(s.top());
             s.pop();
         }
-
-        return ans;
+        return v;
     }
 };
