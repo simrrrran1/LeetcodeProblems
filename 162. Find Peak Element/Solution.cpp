475325a52aed9a1a4d49649c8807aec0212c6310
Harsimranjot <91774565+simrrrran1@users.noreply.github.com>
2025-06-06 00:07:45 +0530

    C++ Solution
          Submission Statistics:
          Question #: 162
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 12.664 MB
          Memory percentile: 31.49

--- 162. Find Peak Element/Solution.cpp
+++ 162. Find Peak Element/Solution.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        if(nums.size() == 1) return 0;
+        // If there's only one element, it is the peak by default
+        if (nums.size() == 1) return 0;
 
         int left = 0, right = nums.size() - 1;
 
-        while(left <= right){
-            int mid = left + ((right-left)/2);
+        // Binary search loop
+        while (left <= right) {
+            int mid = left + ((right - left) / 2);
 
-            if(mid > 0 && nums[mid-1] > nums[mid]) right = mid - 1;
-
-            else if(mid < nums.size()-1 && nums[mid+1] > nums[mid]) left = mid + 1;
-
-            else return mid;
+            // Case 1: If the mid element is less than its left neighbor,
+            // then the peak must be on the left side
+            if (mid > 0 && nums[mid - 1] > nums[mid]) {
+                right = mid - 1;
+            }
+            // Case 2: If the mid element is less than its right neighbor,
+            // then the peak must be on the right side
+            else if (mid < nums.size() - 1 && nums[mid + 1] > nums[mid]) {
+                left = mid + 1;
+            }
+            // Case 3: If mid is greater than both neighbors or at the edge, it's a peak
+            else {
+                return mid;
+            }
         }
+
+        // This line should theoretically never be reached
         return -1;
     }
 };
